afaeaade2a6245ec2cc50fbb1ef8f40841df064a
Sendou <sendou77@gmail.com>
2019-05-05 21:33:06 +0300

    added build

---
+++ build/precache-manifest.9c2970ab19939ec2b3ea214716e54fe9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2888015a4b3a54fccc9f3a16f77b7756",
+    "url": "/index.html"
+  },
+  {
+    "revision": "871ac5748ff5c978e848",
+    "url": "/static/js/2.a93ba1fd.chunk.js"
+  },
+  {
+    "revision": "4bb79fe496bf6dbdbe0e",
+    "url": "/static/js/main.ac2727ea.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
