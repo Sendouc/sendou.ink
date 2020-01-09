079d0bf4e4cad81452dec664c5b6894fc2ddfb7f
Sendou <sendou77@gmail.com>
2020-01-10 00:23:19 +0200

    hotkeys for map planner tools

--- frontend-react/src/components/plans/ToolsSelector.js
+++ frontend-react/src/components/plans/ToolsSelector.js
@@ -1,10 +1,16 @@
 import React, { useState } from "react"
 import Draggable from "react-draggable"
-import { Icon } from "semantic-ui-react"
+import { Icon, Popup } from "semantic-ui-react"
 import { Tools } from "@sendou/react-sketch"
+import { useHotkeys } from "react-hotkeys-hook"
 
 const ToolsSelector = ({ tool, setTool }) => {
   const [activeDrags, setActiveDrags] = useState(0)
+  useHotkeys("p", () => setTool(Tools.Pencil))
+  useHotkeys("l", () => setTool(Tools.Line))
+  useHotkeys("r", () => setTool(Tools.Rectangle))
+  useHotkeys("c", () => setTool(Tools.Circle))
+  useHotkeys("s", () => setTool(Tools.Select))
 
   const onStart = () => {
     setActiveDrags(activeDrags + 1)
@@ -40,50 +46,80 @@ const ToolsSelector = ({ tool, setTool }) => {
           </div>
         </strong>
         <div>
-          <Icon
-            name="pencil"
-            color="blue"
-            size="big"
-            bordered
-            inverted={tool === Tools.Pencil}
-            onClick={() => setTool(Tools.Pencil)}
-            style={{ cursor: "pointer", marginTop: "0.1em" }}
+          <Popup
+            header="Pencil"
+            content="Shortcut key: P"
+            trigger={
+              <Icon
+                name="pencil"
+                color="blue"
+                size="big"
+                bordered
+                inverted={tool === Tools.Pencil}
+                onClick={() => setTool(Tools.Pencil)}
+                style={{ cursor: "pointer", marginTop: "0.1em" }}
+              />
+            }
           />
-          <Icon
-            name="window minimize outline"
-            color="blue"
-            size="big"
-            bordered
-            inverted={tool === Tools.Line}
-            onClick={() => setTool(Tools.Line)}
-            style={{ cursor: "pointer", marginTop: "0.1em" }}
+          <Popup
+            header="Line"
+            content="Shortcut key: L"
+            trigger={
+              <Icon
+                name="window minimize outline"
+                color="blue"
+                size="big"
+                bordered
+                inverted={tool === Tools.Line}
+                onClick={() => setTool(Tools.Line)}
+                style={{ cursor: "pointer", marginTop: "0.1em" }}
+              />
+            }
           />
-          <Icon
-            name="square outline"
-            color="blue"
-            size="big"
-            bordered
-            inverted={tool === Tools.Rectangle}
-            onClick={() => setTool(Tools.Rectangle)}
-            style={{ cursor: "pointer", marginTop: "0.1em" }}
+          <Popup
+            header="Rectangle"
+            content="Shortcut key: R"
+            trigger={
+              <Icon
+                name="square outline"
+                color="blue"
+                size="big"
+                bordered
+                inverted={tool === Tools.Rectangle}
+                onClick={() => setTool(Tools.Rectangle)}
+                style={{ cursor: "pointer", marginTop: "0.1em" }}
+              />
+            }
           />
-          <Icon
-            name="circle outline"
-            color="blue"
-            size="big"
-            bordered
-            inverted={tool === Tools.Circle}
-            onClick={() => setTool(Tools.Circle)}
-            style={{ cursor: "pointer", marginTop: "0.1em" }}
+          <Popup
+            header="Circle"
+            content="Shortcut key: C"
+            trigger={
+              <Icon
+                name="circle outline"
+                color="blue"
+                size="big"
+                bordered
+                inverted={tool === Tools.Circle}
+                onClick={() => setTool(Tools.Circle)}
+                style={{ cursor: "pointer", marginTop: "0.1em" }}
+              />
+            }
           />
-          <Icon
-            name="object group outline"
-            color="blue"
-            size="big"
-            bordered
-            inverted={tool === Tools.Select}
-            onClick={() => setTool(Tools.Select)}
-            style={{ cursor: "pointer", marginTop: "0.1em" }}
+          <Popup
+            header="Select"
+            content="Shortcut key: S"
+            trigger={
+              <Icon
+                name="object group outline"
+                color="blue"
+                size="big"
+                bordered
+                inverted={tool === Tools.Select}
+                onClick={() => setTool(Tools.Select)}
+                style={{ cursor: "pointer", marginTop: "0.1em" }}
+              />
+            }
           />
         </div>
       </div>
