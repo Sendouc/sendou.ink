09b0c88b2ffc7c151a25c42ab3491f72973c1e74
Sendou <38327916+Sendouc@users.noreply.github.com>
2020-05-01 15:07:38 +0300

    footer new style

--- frontend-react/src/components/root/Footer.tsx
+++ frontend-react/src/components/root/Footer.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from "react"
-import { Link, Image, Box } from "@chakra-ui/core"
+import { Link, Image, Box, Flex } from "@chakra-ui/core"
 import { Link as ReachLink } from "@reach/router"
 import { footerSquid, footerOcto } from "../../assets/imageImports"
 import MyThemeContext from "../../themeContext"
@@ -12,27 +12,10 @@ const Footer: React.FC = () => {
     Math.random() > 0.5 ? footerSquid : footerOcto
   )
   const { themeColorWithShade, colorMode } = useContext(MyThemeContext)
-  const isSmall = useBreakPoints(480)
 
   return (
-    <Box mt="2em">
+    <Box mt="2em" color={colorMode === "light" ? "white" : "black"}>
       <Box display="flex" alignItems="flex-end">
-        {!isSmall && (
-          <>
-            <Link href="https://discord.gg/J6NqUvt" mx="1em">
-              <IconButton icon={FaDiscord} aria-label="Link to Discord" />
-            </Link>
-            <Link href="https://twitter.com/sendouc" mx="1em">
-              <IconButton icon={FaTwitter} aria-label="Link to Twitter" />
-            </Link>
-            <Link href="https://www.twitch.tv/sendou" mx="1em">
-              <IconButton icon={FaTwitch} aria-label="Link to Twitch" />
-            </Link>
-            <Link href="https://github.com/Sendouc/sendou-ink" mx="1em">
-              <IconButton icon={FaGithub} aria-label="Link to Github" />
-            </Link>
-          </>
-        )}
         <Image
           src={footerBojoing[colorMode]}
           bg={themeColorWithShade}
@@ -43,8 +26,7 @@ const Footer: React.FC = () => {
           userSelect="none"
         />
       </Box>
-      <Box
-        display="flex"
+      <Flex
         bg={themeColorWithShade}
         p="25px"
         flexShrink={0}
@@ -53,49 +35,31 @@ const Footer: React.FC = () => {
         fontWeight="bold"
         letterSpacing="1px"
         flexWrap="wrap"
-        justifyContent={isSmall ? "center" : undefined}
+        justifyContent="space-between"
       >
-        {isSmall && (
-          <Box
-            display="flex"
-            alignItems="center"
-            flexWrap="wrap"
-            justifyContent="center"
-            color="black"
-          >
-            <Link href="https://discord.gg/J6NqUvt" m="1em">
-              <Box as={FaDiscord} size="50px" />
-            </Link>
-            <Link href="https://twitter.com/sendouc" m="1em">
-              <Box as={FaTwitter} size="50px" />
-            </Link>
-            <Link href="https://www.twitch.tv/sendou" m="1em">
-              <Box as={FaTwitch} size="50px" />
-            </Link>
-            <Link href="https://github.com/Sendouc/sendou-ink" m="1em">
-              <Box as={FaGithub} size="50px" />
-            </Link>
-          </Box>
-        )}
-        <ReachLink to="/about">
-          <Box
-            mx="2em"
-            my="1em"
-            color={colorMode === "light" ? "#fffffe" : "#0d0d0d"}
-          >
-            About
-          </Box>
-        </ReachLink>
-        <ReachLink to="/links">
-          <Box
-            mx="2em"
-            my="1em"
-            color={colorMode === "light" ? "#fffffe" : "#0d0d0d"}
-          >
-            External links
-          </Box>
-        </ReachLink>
-      </Box>
+        <Flex flexWrap="wrap" justifyContent="space-between">
+          <Link href="/about">
+            <a style={{ marginRight: "1em" }}>About</a>
+          </Link>
+          <Link href="/links">
+            <a>External links</a>
+          </Link>
+        </Flex>
+        <Flex alignItems="center" flexWrap="wrap" justifyContent="center">
+          <a href="https://discord.gg/J6NqUvt">
+            <Box as={FaDiscord} size="30px" m="1em" />
+          </a>
+          <a href="https://twitter.com/sendouc">
+            <Box as={FaTwitter} size="30px" m="1em" />
+          </a>
+          <a href="https://www.twitch.tv/sendou">
+            <Box as={FaTwitch} size="30px" m="1em" />
+          </a>
+          <a href="https://github.com/Sendouc/sendou-ink">
+            <Box as={FaGithub} size="30px" m="1em" />
+          </a>
+        </Flex>
+      </Flex>
     </Box>
   )
 }
