beb40c33539899c251fa22cea43350857d68b04f
Sendou <38327916+Sendouc@users.noreply.github.com>
2020-05-01 14:45:31 +0300

    removed console log

--- graphql-schemas/plus.js
+++ graphql-schemas/plus.js
@@ -279,8 +279,6 @@ const resolvers = {
           )
         })
 
-      console.log("suggested", suggested)
-
       const date = new Date()
       const year = date.getFullYear()
       const month = date.getMonth() + 1
