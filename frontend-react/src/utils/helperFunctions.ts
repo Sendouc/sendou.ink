8db50db8b71259b897f8bda1a6557a13704c7e00
Sendou <38327916+Sendouc@users.noreply.github.com>
2020-05-02 14:27:58 +0300

    new week number algo

--- frontend-react/src/utils/helperFunctions.ts
+++ frontend-react/src/utils/helperFunctions.ts
@@ -37,10 +37,22 @@ export const removeFalsy = (obj: { [key: string]: any }) => {
   return newObj
 }
 
-// https://stackoverflow.com/a/31810991
-export const getWeek = (date: Date) => {
-  const onejan = new Date(date.getFullYear(), 0, 1)
-  const today = new Date(date.getFullYear(), date.getMonth(), date.getDate())
-  const dayOfYear = (today.getTime() - onejan.getTime() + 86400000) / 86400000
-  return Math.ceil(dayOfYear / 7)
+// https://weeknumber.net/how-to/javascript
+export const getWeek = (dateInput: Date) => {
+  const date = new Date(dateInput.getTime())
+  date.setHours(0, 0, 0, 0)
+  // Thursday in current week decides the year.
+  date.setDate(date.getDate() + 3 - ((date.getDay() + 6) % 7))
+  // January 4 is always in week 1.
+  const week1 = new Date(date.getFullYear(), 0, 4)
+  // Adjust to Thursday in week 1 and count number of weeks from date to week1.
+  return (
+    1 +
+    Math.round(
+      ((date.getTime() - week1.getTime()) / 86400000 -
+        3 +
+        ((week1.getDay() + 6) % 7)) /
+        7
+    )
+  )
 }
