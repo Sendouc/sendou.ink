02e4206694ceb5e4b817d67a1a4fd98a0d2c78e1
Kalle (Sendou) <38327916+Sendouc@users.noreply.github.com>
2020-09-14 02:25:56 +0300

    remove unused deps

--- frontend-react/src/index.tsx
+++ frontend-react/src/index.tsx
@@ -1,23 +1,15 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import App from "./components/root/App"
+import { ApolloProvider } from "@apollo/react-hooks"
 import { ChakraProvider } from "@chakra-ui/core"
 import theme from "@chakra-ui/theme"
-import { ApolloProvider } from "@apollo/react-hooks"
-import { QueryParamProvider } from "use-query-params"
-import { HelmetProvider } from "react-helmet-async"
-import { LocationProvider, createHistory } from "@reach/router"
-import * as Sentry from "@sentry/react"
+import { createHistory, LocationProvider } from "@reach/router"
 import ApolloClient from "apollo-boost"
-import * as serviceWorker from "./serviceWorker"
+import React from "react"
+import ReactDOM from "react-dom"
+import { HelmetProvider } from "react-helmet-async"
+import { QueryParamProvider } from "use-query-params"
+import App from "./components/root/App"
 import "./i18n"
-
-if (process.env.NODE_ENV === "production") {
-  Sentry.init({
-    dsn:
-      "https://fbdedefb54744b33aa0450f1f9f52ee9@o443322.ingest.sentry.io/5416867",
-  })
-}
+import * as serviceWorker from "./serviceWorker"
 
 const client = new ApolloClient({
   uri:
