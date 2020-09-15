290f8ec1c3d5c2934a7db95854ade9481350c11e
Kalle (Sendou) <38327916+Sendouc@users.noreply.github.com>
2020-09-15 15:06:17 +0300

    calendar page new

--- frontend-react/src/components/calendar/CalendarPage.tsx
+++ frontend-react/src/components/calendar/CalendarPage.tsx
@@ -1,14 +1,12 @@
 import { useQuery } from "@apollo/react-hooks"
-import { Box, Divider, Flex, Avatar } from "@chakra-ui/core"
+import { Box } from "@chakra-ui/core"
 import { RouteComponentProps, useLocation } from "@reach/router"
 import React, { useContext, useEffect, useRef, useState } from "react"
 import { Helmet } from "react-helmet-async"
 import { Trans, useTranslation } from "react-i18next"
-import { FaFilter } from "react-icons/fa"
 import {
   UpcomingEventsData,
   UPCOMING_EVENTS,
-  CompetitiveFeedEvent,
 } from "../../graphql/queries/upcomingEvents"
 import MyThemeContext from "../../themeContext"
 import { getWeek } from "../../utils/helperFunctions"
@@ -18,57 +16,17 @@ import PageHeader from "../common/PageHeader"
 import SubHeader from "../common/SubHeader"
 import Input from "../elements/Input"
 import TournamentInfo from "./TournamentInfo"
-import plusServerImage from "../../assets/plusServer.jpg"
-import { UserData } from "../../types"
-import { USER } from "../../graphql/queries/user"
-
-const getFirstFridayDate = () => {
-  const today = new Date()
-  const month =
-    today.getDate() <= 7 && today.getDay() <= 5
-      ? today.getMonth()
-      : today.getMonth() + 1
-
-  let day = 1
-  while (day <= 7) {
-    const dateOfVoting = new Date(
-      Date.UTC(today.getFullYear(), month, day, 15, 0, 0)
-    )
-
-    if (dateOfVoting.getDay() === 5) return "" + dateOfVoting.getTime()
-
-    day++
-  }
-
-  console.error("Couldn't resolve first friday of the month for voting")
-  return "" + new Date(2000, 1, 1).getTime()
-}
-
-const PLUS_VOTING_TEMPLATE: CompetitiveFeedEvent = {
-  name: "",
-  date: getFirstFridayDate(),
-  description: "",
-  message_url: "",
-  message_discord_id: "",
-  discord_invite_url: "",
-  poster_discord_user: {
-    username: "",
-    discriminator: "",
-    discord_id: "",
-  },
-  isVotingTemplate: true,
-}
 
 const CalendarPage: React.FC<RouteComponentProps> = () => {
-  const { darkerBgColor, grayWithShade } = useContext(MyThemeContext)
-  const { t, i18n } = useTranslation()
+  const { grayWithShade } = useContext(MyThemeContext)
+  const { t } = useTranslation()
+
+  const [filter, setFilter] = useState("")
+
   const location = useLocation()
   const matchingRef = useRef<HTMLDivElement>(null)
 
   const { data, error, loading } = useQuery<UpcomingEventsData>(UPCOMING_EVENTS)
-  const { data: userData } = useQuery<UserData>(USER)
-
-  const [filter, setFilter] = useState("")
 
   const searchParamsEventName = new URLSearchParams(location.search).get("name")
 
@@ -84,9 +42,9 @@ const CalendarPage: React.FC<RouteComponentProps> = () => {
   const events = data!.upcomingEvents
 
   let lastPrintedWeek: number | null = null
-  let lastPrintedDay: number | null = null
-  let lastPrintedMonth: number | null = null
   const thisWeekNumber = getWeek(new Date())
+
+  const timeNow = new Date().toTimeString()
 
   const filteredTournaments = events.filter(
     (event) =>
@@ -95,15 +53,6 @@ const CalendarPage: React.FC<RouteComponentProps> = () => {
         .toLowerCase()
         .includes(filter.toLowerCase())
   )
-
-  const isPlusServerMember = !!userData?.user?.plus?.membership_status
-
-  if (isPlusServerMember) {
-    filteredTournaments.push(PLUS_VOTING_TEMPLATE)
-    filteredTournaments.sort((a, b) => parseInt(a.date) - parseInt(b.date))
-  }
-
-  const timeNow = new Date().toTimeString()
 
   return (
     <>
@@ -115,29 +64,21 @@ const CalendarPage: React.FC<RouteComponentProps> = () => {
         value={filter}
         setValue={(value) => setFilter(value)}
         label={t("calendar;Filter tournaments")}
-        icon={FaFilter}
+        m="3rem 0 2rem"
       />
       {filteredTournaments.map((event) => {
         const time = new Date(parseInt(event.date))
         const weekNumber = getWeek(time)
-        const thisDay = time.getDate()
-        const thisMonth = time.getMonth()
         const printWeekHeader = weekNumber !== lastPrintedWeek
 
-        const printDayHeader =
-          thisDay !== lastPrintedDay || thisMonth !== lastPrintedMonth
         if (printWeekHeader) {
           lastPrintedWeek = weekNumber
         }
-        if (printDayHeader) {
-          lastPrintedDay = thisDay
-          lastPrintedMonth = thisMonth
-        }
 
         return (
-          <React.Fragment key={event.discord_invite_url}>
-            {printWeekHeader ? (
-              <Box my="2em">
+          <React.Fragment key={event.name}>
+            {printWeekHeader && (
+              <Box my={10}>
                 <SubHeader>
                   <Trans i18nKey="calendar;weekNumber">
                     Week {{ weekNumber }}
@@ -147,49 +88,21 @@ const CalendarPage: React.FC<RouteComponentProps> = () => {
                   )}
                 </SubHeader>
               </Box>
-            ) : (
-              <Divider my="2em" />
             )}
-            {printDayHeader && (
-              <Flex
-                bg={darkerBgColor}
-                borderRadius="5px"
-                p="0.5em"
-                my="1.5em"
-                alignItems="center"
-              >
-                {new Date(parseInt(event.date)).toLocaleString(i18n.language, {
-                  weekday: "long",
-                  month: "long",
-                  day: "numeric",
-                })}
-              </Flex>
-            )}
-            {event.isVotingTemplate ? (
-              <Flex alignItems="center">
-                <Avatar size="sm" src={plusServerImage} mr="0.5em" />{" "}
-                <Trans i18nKey="calendar;plusVotingInfo">
-                  <b style={{ marginRight: "0.2em" }}>Plus Server voting</b> of
-                  the month starts and lasts for the weekend
-                </Trans>
-              </Flex>
-            ) : (
-              <div
-                ref={
-                  event.name === searchParamsEventName ? matchingRef : undefined
-                }
-              >
-                <TournamentInfo
-                  tournament={event}
-                  date={time}
-                  expandedByDefault={event.name === searchParamsEventName}
-                />
-              </div>
-            )}
+            <div
+              ref={
+                event.name === searchParamsEventName ? matchingRef : undefined
+              }
+            >
+              <TournamentInfo
+                tournament={event}
+                date={time}
+                expandedByDefault={event.name === searchParamsEventName}
+              />
+            </div>
           </React.Fragment>
         )
       })}
-      <Divider my="2em" />
       <Box color={grayWithShade}>
         <Trans i18nKey="calendar;footer">
           All events listed in your local time: {{ timeNow }}
