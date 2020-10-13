6753b7e29c4b51db675d1b49bff1361537a8930c
Kalle (Sendou) <38327916+Sendouc@users.noreply.github.com>
2020-10-13 19:07:22 +0300

    seed user script

---
+++ prisma/seed.ts
@@ -0,0 +1,36 @@
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+const main = async () => {
+  await prisma.user.create({
+    data: {
+      username: "N-ZAP",
+      discriminator: "6227",
+      discordId: "455039198672453645",
+      discordAvatar: "f809176af93132c3db5f0a5019e96339",
+      profile: {
+        create: {
+          bio: "My cool bio! Supports markdown too: **bolded**",
+          country: "US",
+          customUrlPath: "tester",
+          sensMotion: 45,
+          sensStick: -20,
+          weaponPool: ["Tenta Brella", "Range Blaster", "Luna Blaster", "N-ZAP '89"],
+          twitchName: "nintendo",
+          youtubeId: "UCAtobAxsQcACwDZCSH9uJfA"
+        }
+      }
+    }
+  })
+
+  console.log("User created")
+}
+
+main()
+  .catch(e => console.error(e))
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
+
+export { }
