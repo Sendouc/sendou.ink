684756e6f5a5846c9c85643c40e88311d58359ca
Kalle (Sendou) <38327916+Sendouc@users.noreply.github.com>
2020-10-17 18:49:44 +0300

    utilize chakra global theming

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,45 @@
 import { ApolloProvider } from "@apollo/client";
-import { ChakraProvider } from "@chakra-ui/core";
+import { ChakraProvider, extendTheme } from "@chakra-ui/core";
+import { mode } from "@chakra-ui/theme-tools";
 import { useApollo } from "lib/apollo";
+import GoogleFonts from "next-google-fonts";
 import type { AppProps } from "next/app";
 import Layout from "scenes/Layout";
+import { theme } from "theme";
+
+const extendedTheme = extendTheme({
+  styles: {
+    global: (props) => ({
+      body: {
+        fontFamily: "'Rubik', sans-serif",
+        color: mode(theme.light.textColor, theme.dark.textColor)(props),
+        bg: mode(theme.light.bgColor, theme.dark.bgColor)(props),
+      },
+    }),
+  },
+  /*components: {
+    Button: {
+      defaultProps: {
+        colorScheme: theme,
+      },
+    },
+  },*/
+});
 
 const MyApp = (props: AppProps) => {
   const apolloClient = useApollo(props.pageProps.initialApolloState);
 
-  // FIXME: on initial load components render twice
   return (
-    <ApolloProvider client={apolloClient}>
-      <ChakraProvider>
-        <Layout {...props} />
-      </ChakraProvider>
-    </ApolloProvider>
+    <>
+      <GoogleFonts
+        href={`https://fonts.googleapis.com/css2?family=Rubik&display=swap`}
+      />
+      <ApolloProvider client={apolloClient}>
+        <ChakraProvider theme={extendedTheme}>
+          <Layout {...props} />
+        </ChakraProvider>
+      </ApolloProvider>
+    </>
   );
 };
 
--- scenes/Layout/index.tsx
+++ scenes/Layout/index.tsx
@@ -1,8 +1,6 @@
 import { Container, Flex } from "@chakra-ui/core";
-import { useMyTheme } from "lib/useMyTheme";
 import { AppProps } from "next/app";
 import { useRouter } from "next/dist/client/router";
-import { useEffect } from "react";
 import Footer from "./components/Footer";
 import IconNavBar from "./components/IconNavBar";
 import TopNav from "./components/TopNav";
@@ -16,19 +14,13 @@ const PAGES_WITH_WIDE_CONTAINER = [
 ];
 
 const Layout = ({ Component, pageProps }: AppProps) => {
-  const { bgColor, textColor } = useMyTheme();
   const pathname = useRouter().pathname;
-
-  // FIXME
-  useEffect(() => {
-    document.body.style.backgroundColor = bgColor;
-  }, [bgColor]);
 
   return (
     <>
       <TopNav />
       <IconNavBar />
-      <Flex flexDirection="column" color={textColor} minH="100vh" pt="1rem">
+      <Flex flexDirection="column" minH="100vh" pt="1rem">
         <Container
           maxWidth={
             PAGES_WITH_WIDE_CONTAINER.includes(pathname) ? "120ch" : "70ch"
