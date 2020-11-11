a6c88c02aa9b3eb3e505edffd09f4859de58c3f5
Kalle (Sendou) <38327916+Sendouc@users.noreply.github.com>
2020-11-11 13:32:13 +0200

    update more deps

--- lingui.config.js
+++ lingui.config.js
@@ -1,17 +1,26 @@
 module.exports = {
-  locales: ["de", "en", "es", "fr", "it", "nl", "pt", "sv", "el", "ru", "ja", "ko", "zh"],
+  locales: [
+    "de",
+    "en",
+    "es",
+    "fr",
+    "it",
+    "nl",
+    "pt",
+    "sv",
+    "el",
+    "ru",
+    "ja",
+    "ko",
+    "zh",
+  ],
   sourceLocale: "en",
-  fallbackLocale: "en",
+  fallbackLocales: "en",
   orderBy: "origin",
   catalogs: [
     {
       path: "<rootDir>/locale/{locale}/messages",
-      include: [
-        "./pages/",
-        "./components/",
-        "./scenes/",
-        "./lib/"
-      ],
+      include: ["./pages/", "./components/", "./scenes/", "./lib/"],
     },
   ],
-}
+};
