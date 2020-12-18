6341361e3f90178060e6349d49b20e8315715a60
Kalle (Sendou) <38327916+Sendouc@users.noreply.github.com>
2020-12-18 23:36:31 +0200

    closes #225 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,56 @@
+import { Box, Flex, Heading, Image } from "@chakra-ui/react";
+import { useMyTheme } from "lib/useMyTheme";
+
+const splatoonOneMaps = [
+  {
+    image: "alfonsino",
+    name: "Museum D'Alfonsino",
+  },
+  {
+    image: "bluefin",
+    name: "Bluefin Depot",
+  },
+  {
+    image: "bridge",
+    name: "Hammerhead Bridge",
+  },
+  {
+    image: "flounder",
+    name: "Flounder Heights",
+  },
+  {
+    image: "resort",
+    name: "Mahi-Mahi Resort",
+  },
+  {
+    image: "rig",
+    name: "Saltspray Rig",
+  },
+  {
+    image: "underpass",
+    name: "Urchin Underpass",
+  },
+] as const;
+
+const NotFound = () => {
+  const { gray } = useMyTheme();
+  const mapObject =
+    splatoonOneMaps[Math.floor(Math.random() * splatoonOneMaps.length)];
+  return (
+    <Flex flexDirection="column" justifyContent="center" alignItems="center">
+      <Image
+        src={`/splatoon1Maps/${mapObject.image}.png`}
+        borderRadius="5px"
+        w="500px"
+        h="auto"
+        my={4}
+      />
+      <Heading>404 - Not Found</Heading>
+      <Box color={gray}>
+        ...just like {mapObject.name} can't be found in Splatoon 2
+      </Box>
+    </Flex>
+  );
+};
+
+export default NotFound;
