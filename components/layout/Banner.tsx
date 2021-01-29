0f235325eab5697ce4a071e0aa82aa38b333e0d8
Kalle (Sendou) <38327916+Sendouc@users.noreply.github.com>
2021-01-29 12:11:43 +0200

    add ggg to banner

--- components/layout/Banner.tsx
+++ components/layout/Banner.tsx
@@ -2,18 +2,19 @@ import { Box, Image } from "@chakra-ui/react";
 import MyContainer from "components/common/MyContainer";
 
 const event = {
-  bg: "#121621",
-  logoUrl: "https://abload.de/img/rsz_hxznxtrwdpjke.png",
-  staleAfter: "2021-01-10",
+  bg: "#3F3F3F",
+  logoUrl: "https://abload.de/img/screenshot2021-01-29agyjt6.png",
+  staleAfter: "2021-02-08", // always +1 from when the event ends?,
+  link: "https://twitter.com/NineWholeGrains/status/1353110481721040897",
   content:
-    "Who will be crowned the TASL Season 2 champion - FTWin, FreeZe, Rift or Starburst? Catch the playoffs this Saturday at 12PM ET/6PM CET on EndGameTV!",
+    "Grand Graining Grounds is happening on February 6th and 7th. Starting prize pool of $2500!",
 } as const;
 
 const Banner = () => {
   if (new Date().getTime() > new Date(event.staleAfter).getTime()) return null;
 
   return (
-    <a href="https://www.twitch.tv/endgametv1">
+    <a href={event.link}>
       <Box
         bg={event.bg}
         color="white"
