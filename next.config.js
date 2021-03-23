3f7d6874aef4dd415b43fa461024d158384e1d3c
Kalle (Sendou) <38327916+Sendouc@users.noreply.github.com>
2021-03-23 13:22:51 +0200

    build analyzer

--- next.config.js
+++ next.config.js
@@ -1,38 +1,43 @@
 const withImages = require("next-images");
-
-module.exports = withImages({
-  async redirects() {
-    return [
-      {
-        source: "/sr",
-        destination: "/sr/leaderboards",
-        permanent: true,
-      },
-    ];
-  },
-  experimental: {
-    optimizeFonts: true,
-  },
-  images: {
-    domains: ["www.countryflags.io"],
-  },
-  // i18n: {
-  //  v-- import from lib/locales
-  //   locales: [
-  //     "de",
-  //     "el",
-  //     "en",
-  //     "es",
-  //     "fr",
-  //     "it",
-  //     "ja",
-  //     "ko",
-  //     "nl",
-  //     "pt",
-  //     "ru",
-  //     "sv",
-  //     "zh",
-  //   ],
-  //   defaultLocale: "en",
-  // },
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
 });
+
+module.exports = withBundleAnalyzer(
+  withImages({
+    async redirects() {
+      return [
+        {
+          source: "/sr",
+          destination: "/sr/leaderboards",
+          permanent: true,
+        },
+      ];
+    },
+    experimental: {
+      optimizeFonts: true,
+    },
+    images: {
+      domains: ["www.countryflags.io"],
+    },
+    // i18n: {
+    //  v-- import from lib/locales
+    //   locales: [
+    //     "de",
+    //     "el",
+    //     "en",
+    //     "es",
+    //     "fr",
+    //     "it",
+    //     "ja",
+    //     "ko",
+    //     "nl",
+    //     "pt",
+    //     "ru",
+    //     "sv",
+    //     "zh",
+    //   ],
+    //   defaultLocale: "en",
+    // },
+  })
+);
