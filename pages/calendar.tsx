b890ada978cd23f9b7f255c04745517245942bbf
Kalle (Sendou) <38327916+Sendouc@users.noreply.github.com>
2021-04-06 20:47:20 +0300

    5.0

--- pages/calendar.tsx
+++ pages/calendar.tsx
@@ -1,14 +1,13 @@
 import CalendarPage from "app/calendar/components/CalendarPage";
 import HeaderBanner from "components/layout/HeaderBanner";
 import { ssr } from "pages/api/trpc/[trpc]";
-import { trpc } from "utils/trpc";
 
 export const getStaticProps = async () => {
   await ssr.prefetchQuery("calendar.events");
 
   return {
     props: {
-      dehydratedState: trpc.dehydrate(),
+      dehydratedState: ssr.dehydrate(),
     },
     revalidate: 60,
   };
