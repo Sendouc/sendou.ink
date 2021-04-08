1bad562254fab863d06b3b265d8cb9db551405bc
Kalle (Sendou) <38327916+Sendouc@users.noreply.github.com>
2021-04-08 18:21:56 +0300

    add rr tg ut event images

--- app/calendar/utils.ts
+++ app/calendar/utils.ts
@@ -92,6 +92,9 @@ const nameToImage = [
   { code: "lobstercrossfire", name: "lobster crossfire" },
   { code: "swimorsink", name: "swim or sink" },
   { code: "idtga", name: "it's dangerous to go alone" },
+  { code: "rr", name: "reef rushdown" },
+  { code: "tg", name: "testing grounds" },
+  { code: "ut", name: "unnamed tournament" },
 ] as const;
 
 /**
