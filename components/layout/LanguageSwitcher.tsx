7c074163f568f6fd21eb4635d04b14c8902412ab
Kalle (Sendou) <38327916+Sendouc@users.noreply.github.com>
2021-04-21 00:11:59 +0300

    language switcher

--- components/layout/LanguageSwitcher.tsx
+++ components/layout/LanguageSwitcher.tsx
@@ -1,26 +1,76 @@
-import { IconButton, useColorMode } from "@chakra-ui/react";
+import {
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuItemOption,
+  MenuList,
+  MenuOptionGroup,
+  useColorMode,
+} from "@chakra-ui/react";
+import { t } from "@lingui/macro";
+import { useLingui } from "@lingui/react";
+import { useMyTheme } from "hooks/common";
+import React from "react";
 import { FiGlobe } from "react-icons/fi";
+import { activateLocale } from "utils/i18n";
 
-const LanguageSwitcher = ({ isMobile }: { isMobile?: boolean }) => {
-  const { colorMode, toggleColorMode } = useColorMode();
+export const languages = [
+  { code: "de", name: "Deutsch" },
+  { code: "en", name: "English" },
+  { code: "es", name: "Español" },
+  { code: "fr", name: "Français" },
+  { code: "it", name: "Italiano" },
+  { code: "nl", name: "Nederlands" },
+  { code: "pt", name: "Português" },
+  { code: "sv", name: "Svenska" },
+  { code: "el", name: "Ελληνικά" },
+  { code: "ru", name: "Русский" },
+  { code: "ja", name: "日本語" },
+  { code: "ko", name: "한국어" },
+  //{ code: "zh", name: "繁體中文" },
+  { code: "he", name: "עברית" },
+] as const;
+
+export const LanguageSwitcher = ({ isMobile }: { isMobile?: boolean }) => {
+  const { colorMode } = useColorMode();
+  const { i18n } = useLingui();
+  const { secondaryBgColor, textColor } = useMyTheme();
 
   return (
-    <IconButton
-      data-cy="color-mode-toggle"
-      aria-label={`Switch to ${colorMode === "light" ? "dark" : "light"} mode`}
-      variant="ghost"
-      color="current"
-      onClick={toggleColorMode}
-      icon={<FiGlobe />}
-      _hover={
-        colorMode === "dark"
-          ? { bg: "white", color: "black" }
-          : { bg: "black", color: "white" }
-      }
-      borderRadius={isMobile ? "50%" : "0"}
-      size={isMobile ? "lg" : "sm"}
-      display={isMobile ? "flex" : ["none", null, null, "flex"]}
-    />
+    <Menu>
+      <MenuButton
+        as={IconButton}
+        data-cy="color-mode-toggle"
+        aria-label="Switch language"
+        variant="ghost"
+        color="current"
+        icon={<FiGlobe />}
+        _hover={
+          colorMode === "dark"
+            ? { bg: "white", color: "black" }
+            : { bg: "black", color: "white" }
+        }
+        borderRadius={isMobile ? "50%" : "0"}
+        size={isMobile ? "lg" : "sm"}
+        display={isMobile ? "flex" : ["none", null, null, "flex"]}
+      />
+      <MenuList bg={secondaryBgColor} color={textColor}>
+        <MenuOptionGroup
+          title={t`Choose language`}
+          value={i18n.locale}
+          onChange={(newLocale) => {
+            window.localStorage.setItem("locale", newLocale as string);
+            activateLocale(newLocale as string);
+          }}
+        >
+          {languages.map((lang) => (
+            <MenuItemOption key={lang.code} value={lang.code}>
+              {lang.name}
+            </MenuItemOption>
+          ))}
+        </MenuOptionGroup>
+      </MenuList>
+    </Menu>
   );
 };
 
