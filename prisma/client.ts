60b6c1c04a5baf8a7919fde4d49bb3c2086bf4ad
Kalle (Sendou) <38327916+Sendouc@users.noreply.github.com>
2021-05-01 15:23:41 +0300

    Add isJP script

--- prisma/client.ts
+++ prisma/client.ts
@@ -4,8 +4,11 @@ import { PrismaClient } from "@prisma/client";
 let prisma: PrismaClient;
 
 if (!global.prisma) {
-  //global.prisma = new PrismaClient({ log: ["query", "info", "warn"] });
-  global.prisma = new PrismaClient();
+  global.prisma = new PrismaClient(
+    process.env.NODE_ENV !== "production"
+      ? { log: ["query", "info", "warn"] }
+      : undefined
+  );
 }
 
 prisma = global.prisma;
