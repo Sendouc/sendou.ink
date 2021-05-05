dc19f7e5e1b27be97ecbfbf3eb0fce5970ff836d
Kalle (Sendou) <38327916+Sendouc@users.noreply.github.com>
2021-05-05 20:34:13 +0300

    Add sub and special weapons to Map Planner
    Closes #469

--- components/plans/ImageAdder.tsx
+++ components/plans/ImageAdder.tsx
@@ -7,6 +7,40 @@ interface ImageAdderProps {
   addImageToSketch: (imgSrc: string) => void;
 }
 
+const SUB_WEAPON_CODES = [
+  "Bomb_Curling",
+  "Bomb_Piyo",
+  "Bomb_Quick",
+  "Bomb_Robo",
+  "Bomb_Splash",
+  "Bomb_Suction",
+  "Bomb_Tako",
+  "Flag",
+  "PointSensor",
+  "PoisonFog",
+  "Shield",
+  "Sprinkler",
+  "TimerTrap",
+];
+
+const SPECIAL_WEAPON_CODES = [
+  "AquaBall",
+  "Jetpack",
+  "LauncherCurling",
+  "LauncherQuick",
+  "LauncherRobo",
+  "LauncherSplash",
+  "LauncherSuction",
+  "RainCloud",
+  "SuperArmor",
+  "SuperBall",
+  "SuperBubble",
+  "SuperLanding",
+  "SuperMissile",
+  "SuperStamp",
+  "WaterCutter",
+];
+
 const ImageAdder = ({ addImageToSketch }: ImageAdderProps) => {
   return (
     <Flex flexWrap="wrap" w="72rem" mt={4} mx="3" justify="center">
@@ -25,6 +59,32 @@ const ImageAdder = ({ addImageToSketch }: ImageAdderProps) => {
         const imgSrc = `/weapons/Grizzco ${grizzcoWeaponClass}.png`;
         return (
           <Box key={grizzcoWeaponClass} m="3px">
+            <Image
+              onClick={() => addImageToSketch(imgSrc)}
+              src={imgSrc}
+              width={32}
+              height={32}
+            />
+          </Box>
+        );
+      })}
+      {SUB_WEAPON_CODES.map((code) => {
+        const imgSrc = `/subs-specials/Wsb_${code}.png`;
+        return (
+          <Box key={code} m="3px">
+            <Image
+              onClick={() => addImageToSketch(imgSrc)}
+              src={imgSrc}
+              width={32}
+              height={32}
+            />
+          </Box>
+        );
+      })}
+      {SPECIAL_WEAPON_CODES.map((code) => {
+        const imgSrc = `/subs-specials/Wsp_${code}.png`;
+        return (
+          <Box key={code} m="3px">
             <Image
               onClick={() => addImageToSketch(imgSrc)}
               src={imgSrc}
