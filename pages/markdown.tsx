11b8652273d70c700df1e244953e5bcec4f523a5
Igor <12849336+DoubleCookies@users.noreply.github.com>
2021-06-18 00:52:58 +0300

    Sub and special weapons for markdown (#549)
    * Add sub and special weapons emoji
    
    * Markdown page new emojis info + Box for every block + same margins for blocks
    
    * fix import

--- pages/markdown.tsx
+++ pages/markdown.tsx
@@ -7,9 +7,11 @@ import { useState } from "react";
 import { abilityMarkdownCodes } from "utils/lists/abilityMarkdownCodes";
 import { gearMarkdownCodes } from "utils/lists/gearMarkdownCodes";
 import { codeToWeapon } from "utils/lists/weaponCodes";
+import { subSpecialWeaponMarkdownCodes } from "utils/lists/subSpecialWeaponMarkdownCodes";
 
 const MarkdownHelpPage = () => {
   const [showWeapons, setShowWeapons] = useState(false);
+  const [showSubSpecialWeapons, setShowSubSpecialWeapons] = useState(false);
   const [showAbilities, setShowAbilities] = useState(false);
   const [showGear, setShowGear] = useState(false);
   return (
@@ -38,7 +40,7 @@ const MarkdownHelpPage = () => {
           We also support certain special emoji. Below listed are the groups and
           few representatives as example:
         </p>
-        <p style={{ marginTop: "0.5em" }}>
+        <Box as="p" mt={8}>
           <b>Mode emoji</b>
           <Flex flexDir="column">
             <Box>
@@ -57,8 +59,8 @@ const MarkdownHelpPage = () => {
               :clam_blitz: -{">"} <Emoji value=":clam_blitz:" />
             </Box>
           </Flex>
-        </p>
-        <Box as="p" mt={6}>
+        </Box>
+        <Box as="p" mt={8}>
           <b>Weapon emoji</b>
           <Flex flexDir="column">
             <Box>
@@ -72,7 +74,7 @@ const MarkdownHelpPage = () => {
               :custom_e-liter_4k_scope: -{">"}{" "}
               <WeaponImage size={32} name="Custom E-liter 4K Scope" />
             </Box>
-            <Box w="250px" mt="0.5em" mb={6}>
+            <Box w="250px" mt="0.5em">
               <Button onClick={() => setShowWeapons(!showWeapons)} size="sm">
                 {showWeapons ? "Hide" : "Show all"}
               </Button>
@@ -86,7 +88,35 @@ const MarkdownHelpPage = () => {
             </Collapse>
           </Flex>
         </Box>
-        <p style={{ marginTop: "0.5em" }}>
+        <Box as="p" mt={8}>
+          <b>Sub and Special weapons emoji</b>
+          <Flex flexDir="column">
+            <Box>
+              :baller: -{">"}{" "}
+              <Emoji value="baller" />
+            </Box>
+            <Box>
+              :ink_armor: -{">"} <Emoji value="ink_armor" />
+            </Box>
+            <Box>
+              :autobomb: -{">"}{" "}
+              <Emoji value="autobomb" />
+            </Box>
+            <Box w="250px" mt="0.5em">
+              <Button onClick={() => setShowSubSpecialWeapons(!showSubSpecialWeapons)} size="sm">
+                {showSubSpecialWeapons ? "Hide" : "Show all"}
+              </Button>
+            </Box>
+            <Collapse in={showSubSpecialWeapons} animateOpacity>
+              <Flex flexDir="column">
+                {Object.keys(subSpecialWeaponMarkdownCodes).map((code) => (
+                  <Box>:{code}:</Box>
+                ))}
+              </Flex>
+            </Collapse>
+          </Flex>
+        </Box>
+        <Box as="p" mt={8}>
           <b>Ability emoji</b>
           <Flex flexDir="column">
             <Box>
@@ -98,12 +128,10 @@ const MarkdownHelpPage = () => {
             <Box>
               :rp: -{">"} <Emoji value=":rp:" />
             </Box>
-            <Box w="250px" my="0.5em">
+            <Box w="250px" mt="0.5em">
               <Button
                 onClick={() => setShowAbilities(!showAbilities)}
                 size="sm"
-                mt="0.5em"
-                mb={6}
               >
                 {showAbilities ? "Hide" : "Show all"}
               </Button>
@@ -116,8 +144,8 @@ const MarkdownHelpPage = () => {
               </Flex>
             </Collapse>
           </Flex>
-        </p>
-        <p style={{ marginTop: "0.5em" }}>
+        </Box>
+        <Box as="p" mt={8}>
           <b>Gear emoji</b>
           <Flex flexDir="column">
             <Box>
@@ -130,12 +158,10 @@ const MarkdownHelpPage = () => {
               :black_norimaki_750s: -{">"}{" "}
               <Emoji value=":black_norimaki_750s:" />
             </Box>
-            <Box w="250px" my="0.5em">
+            <Box w="250px" mt="0.5em">
               <Button
                 onClick={() => setShowGear(!showGear)}
                 size="sm"
-                mt="0.5em"
-                mb={6}
               >
                 {showGear ? "Hide" : "Show all"}
               </Button>
@@ -148,7 +174,7 @@ const MarkdownHelpPage = () => {
               </Flex>
             </Collapse>
           </Flex>
-        </p>
+        </Box>
       </Box>
     </>
   );
