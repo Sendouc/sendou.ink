673fdbdb4ae1aa7187aba45a2c4d641ca91b662e
Kalle (Sendou) <38327916+Sendouc@users.noreply.github.com>
2021-06-20 15:25:44 +0300

    Next.js 11

--- next.config.js
+++ next.config.js
@@ -5,9 +5,6 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 
 module.exports = withBundleAnalyzer(
   withImages({
-    future: {
-      webpack5: true,
-    },
     async redirects() {
       return [
         {
