71858ad1bea95480760024d599f74af6c429d665
Kalle (Sendou) <38327916+Sendouc@users.noreply.github.com>
2021-07-10 12:52:02 +0300

    Optimize loading

--- components/layout/BeautifulDrawingByBorzoic.tsx
+++ components/layout/BeautifulDrawingByBorzoic.tsx
@@ -1,6 +1,6 @@
 import { Image as ChakraImage, useColorMode } from "@chakra-ui/react";
 import randomColor from "randomcolor";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 interface HSL {
   h: number;
@@ -351,8 +351,27 @@ function getFilters(hex: string) {
 const BeautifulDrawingOfBorzoic = ({ type }: { type: "girl" | "boy" }) => {
   const [hexCode, setHexCode] = useState(randomColor());
   const { colorMode } = useColorMode();
+  const [drawingImgSrc, setDrawingImgSrc] = useState(
+    `/layout/new_${type}_${colorMode}.png`
+  );
+  const [drawingLoaded, setDrawingLoaded] = useState(false);
 
   const handleColorChange = () => setHexCode(randomColor());
+
+  useEffect(() => {
+    const loadImage = (imageUrl: string): Promise<string> => {
+      return new Promise((resolve, reject) => {
+        const loadImg = new Image();
+        loadImg.src = imageUrl;
+        loadImg.onload = () => setTimeout(() => resolve(imageUrl));
+        loadImg.onerror = (err) => reject(err);
+      });
+    };
+
+    loadImage(`/layout/new_${type}_${colorMode}.png`)
+      .then((src) => setDrawingImgSrc(src))
+      .catch((err) => console.error("Failed to load images", err));
+  }, [type, colorMode]);
 
   return (
     <>
@@ -364,17 +383,19 @@ const BeautifulDrawingOfBorzoic = ({ type }: { type: "girl" | "boy" }) => {
         justifySelf={type === "girl" ? "flex-start" : "flex-end"}
         gridRow="1"
         alt=""
+        visibility={drawingLoaded ? "visible" : "hidden"}
       />
       <ChakraImage
         onClick={handleColorChange}
         onMouseEnter={handleColorChange}
-        src={`/layout/new_${type}_${colorMode}.png`}
+        src={drawingImgSrc}
         height={["150px", "240px", "300px", "360px"]}
         gridColumn={type === "girl" ? "2 / 3" : "1 / 2"}
         justifySelf={type === "girl" ? "flex-start" : "flex-end"}
         gridRow="1"
         zIndex="10"
         alt=""
+        onLoad={() => setDrawingLoaded(true)}
       />
     </>
   );
