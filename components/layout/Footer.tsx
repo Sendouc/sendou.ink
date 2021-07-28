4783558845cc75f40f51ca9b719730a1e3fa2f13
Kalle (Sendou) <38327916+Sendouc@users.noreply.github.com>
2021-07-28 21:54:47 +0300

    Use new layering strat for footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,21 +1,18 @@
-import { Box, Image, useColorModeValue } from "@chakra-ui/react";
-import { CSSVariables } from "utils/CSSVariables";
+import { Box, Image } from "@chakra-ui/react";
 import { useRouter } from "next/dist/client/router";
+import { CSSVariables } from "utils/CSSVariables";
+import { getFilters } from "utils/getFilters";
 import FooterContent from "./FooterContent";
 import FooterWaves from "./FooterWaves";
 
 const Footer: React.FC = () => {
-  const species = useRouter().asPath.charCodeAt(1) % 2 === 0 ? "squid" : "octo";
-  const footerImageSrc = useColorModeValue(
-    { octo: "b8ing_light", squid: "boing_light" },
-    { octo: "b8ing_dark", squid: "boing_dark" }
-  )[species];
+  const imgBase =
+    useRouter().asPath.charCodeAt(1) % 2 === 0 ? "boing" : "b8ing";
 
   return (
-    <Box as="footer" mt="auto">
+    <Box as="footer" mt="auto" display="grid" gridTemplateColumns="1fr">
       <Image
-        src={`/layout/${footerImageSrc}.png`}
-        bg={CSSVariables.themeColor}
+        src={`/layout/${imgBase}_bg.png`}
         w="80px"
         ml="auto"
         mr="35%"
@@ -24,6 +21,31 @@ const Footer: React.FC = () => {
         userSelect="none"
         loading="lazy"
         alt=""
+        filter={getFilters(
+          getComputedStyle(document.body)
+            .getPropertyValue("--custom-theme-color")
+            .trim() ||
+            getComputedStyle(document.body)
+              .getPropertyValue("--theme-color")
+              .trim()
+        )}
+        gridRow="1"
+        gridColumn="1 / 2"
+        zIndex="1"
+      />
+      <Image
+        src={`/layout/${imgBase}.png`}
+        w="80px"
+        ml="auto"
+        mr="35%"
+        mb="-5.1%"
+        mt="5rem"
+        userSelect="none"
+        loading="lazy"
+        alt=""
+        gridRow="1"
+        gridColumn="1 / 2"
+        zIndex="10"
       />
       <FooterWaves />
       <FooterContent />
