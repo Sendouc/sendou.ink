4a612f75c45418b2fa0fbfbf0145c7c9c201f11f
Kalle (Sendou) <38327916+Sendouc@users.noreply.github.com>
2021-10-02 22:32:24 +0300

    Top bar initial

---
+++ src/lib/components/layout/RainbowLine.svelte
@@ -0,0 +1,35 @@
+<div class="container">
+	<div class="first" />
+	<div class="second" />
+	<div class="third" />
+	<div class="fourth" />
+	<div class="fifth" />
+</div>
+
+<style>
+	.container {
+		display: grid;
+		grid-template-columns: 1fr 2fr 2fr 2fr 1fr;
+		height: 6px;
+	}
+
+	.first {
+		background-color: var(--theme-2);
+	}
+
+	.second {
+		background-color: var(--theme-3);
+	}
+
+	.third {
+		background-color: var(--theme);
+	}
+
+	.fourth {
+		background-color: var(--theme-4);
+	}
+
+	.fifth {
+		background-color: var(--theme-5);
+	}
+</style>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,26 @@
+<script>
+	import RainbowLine from '$lib/components/layout/RainbowLine.svelte';
+	import '../global.css';
+</script>
+
+<main>
+	<nav>
+		<picture>
+			<source srcset="img/logo.webp" type="image/webp" />
+			<img src="img/logo.png" alt="sendou.ink logo" />
+		</picture>
+	</nav>
+	<RainbowLine />
+	<slot />
+</main>
+
+<style>
+	nav {
+		text-align: center;
+		padding: 0.25rem 0;
+	}
+
+	img {
+		height: 2rem;
+	}
+</style>
