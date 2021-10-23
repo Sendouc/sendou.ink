1abb2ae50a3feeed12b61f5d78aab9bc6de7ae3b
Kalle (Sendou) <38327916+Sendouc@users.noreply.github.com>
2021-10-23 19:18:20 +0300

    Prisma initial

---
+++ packages/server/src/prisma/migrations/20211023161707_init/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "discordId" TEXT NOT NULL,
+    "discordName" TEXT NOT NULL,
+    "discordDiscriminator" TEXT NOT NULL,
+    "discordAvatar" TEXT,
+    "discordRefreshToken" TEXT NOT NULL,
+    "twitch" TEXT,
+    "twitter" TEXT,
+    "youtubeId" TEXT,
+    "youtubeName" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_discordId_key" ON "User"("discordId");
