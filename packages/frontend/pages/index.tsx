081df8284eeaa61623bb96336ea4fba1b9e60638
Kalle (Sendou) <38327916+Sendouc@users.noreply.github.com>
2021-10-25 00:41:02 +0300

    Add frontend skeleton

---
+++ packages/frontend/pages/index.tsx
@@ -0,0 +1,21 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        <h1 className="text-6xl font-bold">
+          Welcome to{" "}
+          <a className="text-blue-600" href="https://nextjs.org">
+            Next.js!
+          </a>
+        </h1>
+      </main>
+    </div>
+  );
+}
