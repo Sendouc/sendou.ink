7f45e942259c63e7de5345623a5f60d4f587dc96
Kalle (Sendou) <38327916+Sendouc@users.noreply.github.com>
2021-10-24 16:42:20 +0300

    Tournament and organization migration

---
+++ packages/server/src/prisma/migrations/20211024133808_init/migration.sql
@@ -0,0 +1,62 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "discordId" TEXT NOT NULL,
+    "discordName" TEXT NOT NULL,
+    "discordDiscriminator" TEXT NOT NULL,
+    "discordAvatar" TEXT,
+    "discordRefreshToken" TEXT NOT NULL,
+    "twitch" TEXT,
+    "twitter" TEXT,
+    "youtubeId" TEXT,
+    "youtubeName" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Organization" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "nameForUrl" TEXT NOT NULL,
+    "ownerId" INTEGER NOT NULL,
+    "discordInvite" TEXT NOT NULL,
+    "twitter" TEXT,
+
+    CONSTRAINT "Organization_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Tournament" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "nameForUrl" TEXT NOT NULL,
+    "desription" TEXT,
+    "startTime" TIMESTAMP(3) NOT NULL,
+    "checkInTime" TIMESTAMP(3) NOT NULL,
+    "bannerBackground" TEXT NOT NULL,
+    "bannerTextColor" TEXT NOT NULL,
+    "organizerId" INTEGER NOT NULL,
+
+    CONSTRAINT "Tournament_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_discordId_key" ON "User"("discordId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Organization_nameForUrl_key" ON "Organization"("nameForUrl");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Organization_ownerId_key" ON "Organization"("ownerId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Tournament_nameForUrl_key" ON "Tournament"("nameForUrl");
+
+-- AddForeignKey
+ALTER TABLE "Organization" ADD CONSTRAINT "Organization_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Tournament" ADD CONSTRAINT "Tournament_organizerId_fkey" FOREIGN KEY ("organizerId") REFERENCES "Organization"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
