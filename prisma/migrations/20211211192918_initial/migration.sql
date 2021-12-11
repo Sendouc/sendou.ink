224817f9e8b35d83e9d16d664ce2c2789228b55e
Kalle (Sendou) <38327916+Sendouc@users.noreply.github.com>
2021-12-11 21:31:11 +0200

    Tournament bracket schemas initial migration

--- prisma/migrations/20211211192918_initial/migration.sql
+++ prisma/migrations/20211211192918_initial/migration.sql
@@ -1,6 +1,9 @@
 -- CreateEnum
 CREATE TYPE "Mode" AS ENUM ('TW', 'SZ', 'TC', 'RM', 'CB');
 
+-- CreateEnum
+CREATE TYPE "TeamOrder" AS ENUM ('UPPER', 'LOWER');
+
 -- CreateTable
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
@@ -86,6 +89,55 @@ CREATE TABLE "TrustRelationships" (
 );
 
 -- CreateTable
+CREATE TABLE "TournamentBracket" (
+    "id" TEXT NOT NULL,
+
+    CONSTRAINT "TournamentBracket_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "TournamentRound" (
+    "id" TEXT NOT NULL,
+    "position" INTEGER NOT NULL,
+    "bracketId" TEXT NOT NULL,
+
+    CONSTRAINT "TournamentRound_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "TournamentRoundStage" (
+    "position" INTEGER NOT NULL,
+    "roundId" TEXT NOT NULL,
+    "stageId" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "TournamentMatch" (
+    "id" TEXT NOT NULL,
+    "roundId" TEXT NOT NULL,
+    "winnerDestinationMatchId" TEXT,
+    "loserDestinationMatchId" TEXT,
+
+    CONSTRAINT "TournamentMatch_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "TournamentMatchParticipant" (
+    "order" "TeamOrder" NOT NULL,
+    "teamId" TEXT NOT NULL,
+    "matchId" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "TournamentMatchGameResult" (
+    "matchId" TEXT NOT NULL,
+    "position" INTEGER NOT NULL,
+    "winner" "TeamOrder" NOT NULL,
+    "reporterId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateTable
 CREATE TABLE "_StageToTournament" (
     "A" INTEGER NOT NULL,
     "B" TEXT NOT NULL
@@ -113,6 +165,15 @@ CREATE UNIQUE INDEX "TournamentTeamMember_memberId_tournamentId_key" ON "Tournam
 CREATE UNIQUE INDEX "TrustRelationships_trustGiverId_trustReceiverId_key" ON "TrustRelationships"("trustGiverId", "trustReceiverId");
 
 -- CreateIndex
+CREATE UNIQUE INDEX "TournamentRoundStage_position_roundId_key" ON "TournamentRoundStage"("position", "roundId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "TournamentMatchParticipant_teamId_matchId_key" ON "TournamentMatchParticipant"("teamId", "matchId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "TournamentMatchGameResult_matchId_position_key" ON "TournamentMatchGameResult"("matchId", "position");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "_StageToTournament_AB_unique" ON "_StageToTournament"("A", "B");
 
 -- CreateIndex
@@ -138,6 +199,33 @@ ALTER TABLE "TrustRelationships" ADD CONSTRAINT "TrustRelationships_trustGiverId
 
 -- AddForeignKey
 ALTER TABLE "TrustRelationships" ADD CONSTRAINT "TrustRelationships_trustReceiverId_fkey" FOREIGN KEY ("trustReceiverId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TournamentRound" ADD CONSTRAINT "TournamentRound_bracketId_fkey" FOREIGN KEY ("bracketId") REFERENCES "TournamentBracket"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TournamentRoundStage" ADD CONSTRAINT "TournamentRoundStage_roundId_fkey" FOREIGN KEY ("roundId") REFERENCES "TournamentRound"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TournamentRoundStage" ADD CONSTRAINT "TournamentRoundStage_stageId_fkey" FOREIGN KEY ("stageId") REFERENCES "Stage"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TournamentMatch" ADD CONSTRAINT "TournamentMatch_winnerDestinationMatchId_fkey" FOREIGN KEY ("winnerDestinationMatchId") REFERENCES "TournamentMatch"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TournamentMatch" ADD CONSTRAINT "TournamentMatch_loserDestinationMatchId_fkey" FOREIGN KEY ("loserDestinationMatchId") REFERENCES "TournamentMatch"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TournamentMatch" ADD CONSTRAINT "TournamentMatch_roundId_fkey" FOREIGN KEY ("roundId") REFERENCES "TournamentRound"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TournamentMatchParticipant" ADD CONSTRAINT "TournamentMatchParticipant_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "TournamentTeam"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TournamentMatchParticipant" ADD CONSTRAINT "TournamentMatchParticipant_matchId_fkey" FOREIGN KEY ("matchId") REFERENCES "TournamentMatch"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TournamentMatchGameResult" ADD CONSTRAINT "TournamentMatchGameResult_matchId_fkey" FOREIGN KEY ("matchId") REFERENCES "TournamentMatch"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "_StageToTournament" ADD FOREIGN KEY ("A") REFERENCES "Stage"("id") ON DELETE CASCADE ON UPDATE CASCADE;
