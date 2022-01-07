eb1939998485f16856a1dc28a11f3c882fd39a2e
Kalle (Sendou) <38327916+Sendouc@users.noreply.github.com>
2022-01-07 13:46:45 +0200

    Temp placeholder in UserItem when no avatar

--- app/components/Layout/UserItem.tsx
+++ app/components/Layout/UserItem.tsx
@@ -7,13 +7,19 @@ export function UserItem() {
   const user = useUser();
   const location = useLocation();
 
-  if (user)
+  if (user && user.discordAvatar)
     return (
       <img
         className="layout__avatar"
         src={`https://cdn.discordapp.com/avatars/${user.discordId}/${user.discordAvatar}.png?size=80`}
       />
     );
+
+  // TODO: just show text... my profile?
+  // TODO: also show this if discordAvatar is stale and 404's
+  if (user) {
+    return <div className="layout__header__logo-container" />;
+  }
 
   return (
     <form action={getLogInUrl(location)} method="post" data-cy="log-in-form">
