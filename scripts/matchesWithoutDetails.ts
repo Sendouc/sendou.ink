a70be20a5644d7e7ddca73c02eee211451e906e7
Kalle <38327916+Sendouc@users.noreply.github.com>
2022-03-16 20:16:22 +0200

    Add matches without details script

---
+++ scripts/matchesWithoutDetails.ts
@@ -0,0 +1,50 @@
+import { PrismaClient } from "@prisma/client";
+import { playersWithResults } from "~/core/play/playerInfos/playerInfos.server";
+import fs from "fs";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const matches = await prisma.lfgGroupMatch.findMany({
+    include: {
+      stages: { include: { stage: true } },
+      groups: { include: { members: { include: { user: true } } } },
+    },
+  });
+
+  const result: {
+    id: string;
+    mapList: { mode: string; map: string }[];
+    start_time: string;
+    end_time: string;
+    players: string[];
+  }[] = [];
+
+  for (const match of matches) {
+    const players = playersWithResults(
+      match.groups.flatMap((g) => g.members).flatMap((m) => m.user.discordId)
+    );
+    const mapList = match.stages
+      .sort((a, b) => a.order - b.order)
+      .filter((stage) => stage.winnerGroupId)
+      .map((stage) => ({ map: stage.stage.name, mode: stage.stage.mode }));
+
+    if (!players.length) continue;
+    if (!mapList.length) continue;
+
+    result.push({
+      id: match.id,
+      mapList,
+      start_time: match.createdAt.toISOString(),
+      end_time: new Date(match.createdAt.getTime() + 60 * 60000).toISOString(),
+      players,
+    });
+  }
+
+  fs.writeFileSync("for_lean.json", JSON.stringify(result, null, 2));
+}
+
+main()
+  // eslint-disable-next-line no-console
+  .then(() => console.log("done"))
+  .catch((e) => console.error(e));
