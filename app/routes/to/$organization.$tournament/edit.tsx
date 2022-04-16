24bd2b90fbdd7e663c6386c0ccfbb11a4c189517
Kalle <38327916+Sendouc@users.noreply.github.com>
2022-04-16 11:32:35 +0300

    Remove unused tab

--- app/routes/to/$organization.$tournament/edit.tsx
+++
@@ -1,7 +0,0 @@
-import { Catcher } from "~/components/Catcher";
-
-export default function EditTournamentPage() {
-  return <div>TODO: edit tournament tab</div>;
-}
-
-export const CatchBoundary = Catcher;
