dd601a5308d35b79fd3dc62c55fbb668eb3f6741
Kalle <38327916+Sendouc@users.noreply.github.com>
2022-04-23 16:50:00 +0300

    Close on click outside

--- app/components/Layout/Menu.tsx
+++ app/components/Layout/Menu.tsx
@@ -1,5 +1,6 @@
 import { Link } from "@remix-run/react";
 import { navItems } from "~/constants";
+import { useOnClickOutside } from "~/hooks/common";
 import { layoutIcon } from "~/utils";
 import { discordUrl, gitHubUrl, patreonUrl, twitterUrl } from "~/utils/urls";
 import { Button } from "../Button";
@@ -9,57 +10,63 @@ import { GitHubIcon } from "../icons/Github";
 import { PatreonIcon } from "../icons/Patreon";
 import { TwitterIcon } from "../icons/Twitter";
 import DrawingSection from "./DrawingSection";
+import * as React from "react";
 
 export function Menu({ close }: { close: () => void }) {
+  const ref = React.useRef(null);
+  useOnClickOutside(ref, close);
+
   return (
     <div className="menu">
-      <DrawingSection type="boy" />
-      <div className="menu__middle-container">
-        <div className="menu__top-extras">
-          <div className="menu__logo-container">
-            <img className="menu__logo" src={layoutIcon("logo")} />
-            sendou.ink
+      <div className="menu__inner" ref={ref}>
+        <DrawingSection type="boy" />
+        <div className="menu__middle-container">
+          <div className="menu__top-extras">
+            <div className="menu__logo-container">
+              <img className="menu__logo" src={layoutIcon("logo")} />
+              sendou.ink
+            </div>
+            <Button onClick={close} variant="minimal" aria-label="Close menu">
+              <CrossIcon className="menu__cross-icon" />
+            </Button>
           </div>
-          <Button onClick={close} variant="minimal" aria-label="Close menu">
-            <CrossIcon className="menu__cross-icon" />
-          </Button>
+          <nav className="menu__nav">
+            {navItems.map((navItem) => (
+              <Link
+                key={navItem.name}
+                className="menu__nav__link"
+                to={navItem.disabled ? "/" : navItem.url ?? navItem.name}
+                data-cy={`nav-link-${navItem.name}`}
+                onClick={close}
+              >
+                <img
+                  src={layoutIcon(navItem.name.replace(" ", ""))}
+                  // TODO: fix
+                  className="layout__nav__link__icon"
+                  width="32"
+                  height="32"
+                />
+                {navItem.displayName ?? navItem.name}
+              </Link>
+            ))}
+          </nav>
+          <div className="menu__icons-container">
+            <a href={gitHubUrl()}>
+              <GitHubIcon className="menu__icon" />
+            </a>
+            <a href={discordUrl()}>
+              <DiscordIcon className="menu__icon" />
+            </a>
+            <a href={twitterUrl()}>
+              <TwitterIcon className="menu__icon" />
+            </a>
+            <a href={patreonUrl()}>
+              <PatreonIcon className="menu__icon" />
+            </a>
+          </div>
         </div>
-        <nav className="menu__nav">
-          {navItems.map((navItem) => (
-            <Link
-              key={navItem.name}
-              className="menu__nav__link"
-              to={navItem.disabled ? "/" : navItem.url ?? navItem.name}
-              data-cy={`nav-link-${navItem.name}`}
-              onClick={close}
-            >
-              <img
-                src={layoutIcon(navItem.name.replace(" ", ""))}
-                // TODO: fix
-                className="layout__nav__link__icon"
-                width="32"
-                height="32"
-              />
-              {navItem.displayName ?? navItem.name}
-            </Link>
-          ))}
-        </nav>
-        <div className="menu__icons-container">
-          <a href={gitHubUrl()}>
-            <GitHubIcon className="menu__icon" />
-          </a>
-          <a href={discordUrl()}>
-            <DiscordIcon className="menu__icon" />
-          </a>
-          <a href={twitterUrl()}>
-            <TwitterIcon className="menu__icon" />
-          </a>
-          <a href={patreonUrl()}>
-            <PatreonIcon className="menu__icon" />
-          </a>
-        </div>
+        <DrawingSection type="girl" />
       </div>
-      <DrawingSection type="girl" />
     </div>
   );
 }
