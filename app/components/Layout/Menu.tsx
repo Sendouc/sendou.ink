bade7c10b7eb9d76a76cf182cdc3517c4f1e2dc5
Kalle <38327916+Sendouc@users.noreply.github.com>
2022-04-26 18:27:08 +0300

    Use grid

--- app/components/Layout/Menu.tsx
+++ app/components/Layout/Menu.tsx
@@ -20,50 +20,48 @@ export function Menu({ close }: { close: () => void }) {
     <div className="menu">
       <div className="menu__inner" ref={ref}>
         <DrawingSection type="boy" />
-        <div className="menu__middle-container">
-          <div className="menu__top-extras">
-            <div className="menu__logo-container">
-              <img className="menu__logo" src={layoutIcon("logo")} />
-              sendou.ink
-            </div>
-            <Button onClick={close} variant="minimal" aria-label="Close menu">
-              <CrossIcon className="menu__cross-icon" />
-            </Button>
+        <div className="menu__top-extras">
+          <div className="menu__logo-container">
+            <img className="menu__logo" src={layoutIcon("logo")} />
+            sendou.ink
           </div>
-          <nav className="menu__nav">
-            {navItems.map((navItem) => (
-              <Link
-                key={navItem.name}
-                className="menu__nav__link"
-                to={navItem.disabled ? "/" : navItem.url ?? navItem.name}
-                data-cy={`nav-link-${navItem.name}`}
-                onClick={close}
-              >
-                <img
-                  src={layoutIcon(navItem.name.replace(" ", ""))}
-                  // TODO: fix
-                  className="layout__nav__link__icon"
-                  width="32"
-                  height="32"
-                />
-                {navItem.displayName ?? navItem.name}
-              </Link>
-            ))}
-          </nav>
-          <div className="menu__icons-container">
-            <a href={gitHubUrl()}>
-              <GitHubIcon className="menu__icon" />
-            </a>
-            <a href={discordUrl()}>
-              <DiscordIcon className="menu__icon" />
-            </a>
-            <a href={twitterUrl()}>
-              <TwitterIcon className="menu__icon" />
-            </a>
-            <a href={patreonUrl()}>
-              <PatreonIcon className="menu__icon" />
-            </a>
-          </div>
+          <Button onClick={close} variant="minimal" aria-label="Close menu">
+            <CrossIcon className="menu__cross-icon" />
+          </Button>
+        </div>
+        <nav className="menu__nav">
+          {navItems.map((navItem) => (
+            <Link
+              key={navItem.name}
+              className="menu__nav__link"
+              to={navItem.disabled ? "/" : navItem.url ?? navItem.name}
+              data-cy={`nav-link-${navItem.name}`}
+              onClick={close}
+            >
+              <img
+                src={layoutIcon(navItem.name.replace(" ", ""))}
+                // TODO: fix
+                className="layout__nav__link__icon"
+                width="32"
+                height="32"
+              />
+              {navItem.displayName ?? navItem.name}
+            </Link>
+          ))}
+        </nav>
+        <div className="menu__icons-container">
+          <a href={gitHubUrl()}>
+            <GitHubIcon className="menu__icon" />
+          </a>
+          <a href={discordUrl()}>
+            <DiscordIcon className="menu__icon" />
+          </a>
+          <a href={twitterUrl()}>
+            <TwitterIcon className="menu__icon" />
+          </a>
+          <a href={patreonUrl()}>
+            <PatreonIcon className="menu__icon" />
+          </a>
         </div>
         <DrawingSection type="girl" />
       </div>
