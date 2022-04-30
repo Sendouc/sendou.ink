2911f04a77875530c7721794483452a02fd4cdcb
Kalle <38327916+Sendouc@users.noreply.github.com>
2022-04-30 12:23:48 +0300

    Clean away prisma migrations

--- prisma/migrations/20220409090909_tournament_match_add_number/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "TournamentMatch" ADD COLUMN     "number" INTEGER;
-
--- AddForeignKey
-ALTER TABLE "GameDetail" ADD CONSTRAINT "GameDetail_lfgStageId_fkey" FOREIGN KEY ("lfgStageId") REFERENCES "LfgGroupMatchStage"("id") ON DELETE SET NULL ON UPDATE CASCADE;
