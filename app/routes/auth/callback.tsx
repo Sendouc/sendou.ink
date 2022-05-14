cbc82b8df237b645599ad92c9af89245bc208525
Kalle <38327916+Sendouc@users.noreply.github.com>
2022-05-14 10:44:53 +0300

    Move auth routes to folder

--- app/routes/auth/callback.tsx
+++ app/routes/auth/callback.tsx
@@ -1,11 +1,14 @@
 import { authenticator, DISCORD_AUTH_KEY } from "~/core/authenticator.server";
-import type { LoaderFunction } from "@remix-run/node";
+import { LoaderFunction, redirect } from "@remix-run/node";
+import { userPage } from "~/utils/urls";
 
 export const loader: LoaderFunction = async ({ request }) => {
-  return authenticator.authenticate(DISCORD_AUTH_KEY, request, {
-    successRedirect: "/u/sendou",
+  const data = await authenticator.authenticate(DISCORD_AUTH_KEY, request, {
+    successRedirect: "/",
     // TODO: should include query param that displays an error banner explaining that log in went wrong
     // and where to get help for that
     failureRedirect: "/",
   });
+
+  return redirect(userPage(data.discordId));
 };
