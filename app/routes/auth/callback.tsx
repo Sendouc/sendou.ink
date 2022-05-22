89b4c0a18b85298270b0b578196bb4ec8c9f7e64
Kalle <38327916+Sendouc@users.noreply.github.com>
2022-05-22 20:56:47 +0300

    Remove unused code from authentication callback

--- app/routes/auth/callback.tsx
+++ app/routes/auth/callback.tsx
@@ -1,18 +1,16 @@
+import type { LoaderFunction } from "@remix-run/node";
 import {
   authenticator,
   DISCORD_AUTH_KEY,
 } from "~/core/auth/authenticator.server";
-import type { LoaderFunction } from "@remix-run/node";
-import { redirect } from "@remix-run/node";
-import { userPage } from "~/utils/urls";
 
 export const loader: LoaderFunction = async ({ request }) => {
-  const data = await authenticator.authenticate(DISCORD_AUTH_KEY, request, {
+  await authenticator.authenticate(DISCORD_AUTH_KEY, request, {
     successRedirect: "/",
     // TODO: should include query param that displays an error banner explaining that log in went wrong
     // and where to get help for that
     failureRedirect: "/",
   });
 
-  return redirect(userPage(data.discordId));
+  throw new Response("Unknown authentication state", { status: 500 });
 };
