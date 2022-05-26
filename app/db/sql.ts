eadec24c6df1c9d0c1b520d5d3c93a6ed2939ab8
Kalle <38327916+Sendouc@users.noreply.github.com>
2022-05-26 12:19:32 +0300

    Don't log SQL

--- app/db/sql.ts
+++ app/db/sql.ts
@@ -1,7 +1,6 @@
 import Database from "better-sqlite3";
 
-// eslint-disable-next-line no-console
-export const sql = new Database("db.sqlite3", { verbose: console.log });
+export const sql = new Database("db.sqlite3");
 
 sql.pragma("journal_mode = WAL");
 sql.pragma("foreign_keys = ON");
