e0f224d84ccd193c36c2c61f9b4eaae43dc4fcf3
Kalle <38327916+Sendouc@users.noreply.github.com>
2022-06-12 14:17:05 +0300

    Add small info on SendouQ page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,6 @@
 export default function Index() {
   return (
-    <p className="text-center font-sm">
+    <p className="text-center text-sm">
       Welcome to this work in progress Splatoon 3 version of sendou.ink :){" "}
     </p>
   );
---
+++ app/routes/play.tsx
@@ -0,0 +1,8 @@
+export default function SendouQPage() {
+  return (
+    <p className="text-center text-sm">
+      Splatoon 2 test of SendouQ is over.
+      <br /> SendouQ is planned to return for Splatoon 3.{" "}
+    </p>
+  );
+}
