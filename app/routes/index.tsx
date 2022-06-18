a2f2b2b29914714da5efab004079070f276266fa
Kalle <38327916+Sendouc@users.noreply.github.com>
2022-06-18 16:19:25 +0300

    Add container to front page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,11 @@
+import { Main } from "~/components/Main";
+
 export default function Index() {
   return (
-    <p className="text-center text-sm">
-      Welcome to this work in progress Splatoon 3 version of sendou.ink :){" "}
-    </p>
+    <Main>
+      <p className="text-center text-sm">
+        Welcome to this work in progress Splatoon 3 version of sendou.ink :){" "}
+      </p>
+    </Main>
   );
 }
