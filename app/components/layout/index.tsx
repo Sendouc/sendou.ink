8329c28837e7ebb6481539ae43b14f19d861005f
Kalle <38327916+Sendouc@users.noreply.github.com>
2022-06-29 00:57:18 +0300

    Show current page in header

--- app/components/layout/index.tsx
+++ app/components/layout/index.tsx
@@ -4,17 +4,40 @@ import { HamburgerButton } from "./HamburgerButton";
 import { Menu } from "./Menu";
 import { MobileMenu } from "./MobileMenu";
 import { UserItem } from "./UserItem";
+import navItems from "./nav-items.json";
+import { useLocation } from "@remix-run/react";
 
 export const Layout = React.memo(function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const location = useLocation();
   const [menuOpen, setMenuOpen] = React.useState(false);
+
+  const currentPagesNavItem = navItems.find((navItem) =>
+    location.pathname.includes(navItem.url)
+  );
 
   return (
     <>
       <header className="layout__header">
+        {currentPagesNavItem ? (
+          <h1 className="layout__page-heading">
+            <img
+              className="layout__icon"
+              // xxx: from constant... but this will be compnanet anyway?
+              // xxx: change to avif
+              src={`/img/layout/${currentPagesNavItem.name}.webp`}
+              width="40"
+              height="40"
+              alt=""
+            />
+            {currentPagesNavItem.displayName}
+          </h1>
+        ) : (
+          <div />
+        )}
         <div className="layout__header__right-container">
           <UserItem />
           <HamburgerButton
