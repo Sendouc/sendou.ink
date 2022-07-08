4888fc248cfb11da4c3bd30279bc495e8c78d669
Kalle <38327916+Sendouc@users.noreply.github.com>
2022-07-08 14:27:16 +0300

    Try Cypress in Github Actions 6

--- app/modules/auth/session.server.ts
+++ app/modules/auth/session.server.ts
@@ -3,14 +3,16 @@ import invariant from "tiny-invariant";
 
 const ONE_YEAR_IN_SECONDS = 31_536_000;
 
-invariant(process.env["SESSION_SECRET"]);
+if (process.env.NODE_ENV === "production") {
+  invariant(process.env["SESSION_SECRET"], "SESSION_SECRET is required");
+}
 export const authSessionStorage = createCookieSessionStorage({
   cookie: {
     name: "_session",
     sameSite: "lax",
     path: "/",
     httpOnly: true,
-    secrets: [process.env["SESSION_SECRET"]],
+    secrets: [process.env["SESSION_SECRET"] ?? "secret"],
     secure: process.env.NODE_ENV === "production",
     maxAge: ONE_YEAR_IN_SECONDS,
   },
