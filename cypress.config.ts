e70504942efae75a71f7b672b61018ad90768c96
Kalle <38327916+Sendouc@users.noreply.github.com>
2022-07-08 16:03:15 +0300

    Try Cypress in Github Actions 7

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,4 +6,11 @@ export default defineConfig({
     // setupNodeEvents(on, config) {},
     baseUrl: "http://localhost:4455",
   },
+  video: false,
+  screenshotOnRunFailure: false,
+  retries: {
+    runMode: 2,
+    openMode: 0,
+  },
+  requestTimeout: 5000,
 });
