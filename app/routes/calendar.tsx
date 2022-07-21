b5580c347fb0c3e9e09086ed0be7caaefed00741
Kalle <38327916+Sendouc@users.noreply.github.com>
2022-07-21 20:57:01 +0300

    Calendar navigation with data from loader

--- app/routes/calendar.tsx
+++ app/routes/calendar.tsx
@@ -1,41 +1,112 @@
-import { type LinksFunction } from "@remix-run/node";
-import { Link } from "@remix-run/react";
-import * as React from "react";
+import type { LoaderArgs } from "@remix-run/node";
+import { json, type LinksFunction } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import { addDays, subDays } from "date-fns";
+import { Flipped, Flipper } from "react-flip-toolkit";
+import { z } from "zod";
 import { Main } from "~/components/Main";
 import styles from "~/styles/calendar.css";
+import { dateToWeekNumber, weekNumberToDate } from "~/utils/dates";
+import { actualNumber } from "~/utils/zod";
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: styles }];
 };
 
-export default function CalendarPage() {
-  const [weeks, setWeeks] = React.useState([
-    "10",
-    "Last",
-    "This",
-    "Next",
-    "14",
-  ]);
+const loaderSearchParamsSchema = z.object({
+  week: z.preprocess(actualNumber, z.number().int().min(1).max(53)),
+  year: z.preprocess(
+    actualNumber,
+    z.number().int().min(2022).max(new Date().getFullYear())
+  ),
+});
 
+export const loader = ({ request }: LoaderArgs) => {
+  const url = new URL(request.url);
+  const parsedParams = loaderSearchParamsSchema.safeParse({
+    year: url.searchParams.get("year"),
+    week: url.searchParams.get("week"),
+  });
+
+  const now = new Date();
+  const thisWeek = dateToWeekNumber(now);
+
+  const weekToFetch = parsedParams.success ? parsedParams.data.week : thisWeek;
+  const yearToFetch = parsedParams.success
+    ? parsedParams.data.year
+    : now.getFullYear();
+
+  console.log({ weekToFetch, yearToFetch, thisWeek }); // TODO: db query to get events of this week
+
+  return json({
+    thisWeek,
+    weeks: closeByWeeks({ week: weekToFetch, year: yearToFetch }).map(
+      (week) => ({
+        ...week,
+        numberOfEvents: 12,
+      })
+    ),
+  });
+};
+
+function closeByWeeks(args: { week: number; year: number }) {
+  const dateFromWeekNumber = weekNumberToDate(args);
+
+  return [-4, -3, -2, -1, 0, 1, 2, 3, 4].map((week) => {
+    const date =
+      week < 0
+        ? subDays(dateFromWeekNumber, Math.abs(week) * 7)
+        : addDays(dateFromWeekNumber, week * 7);
+
+    return {
+      number: dateToWeekNumber(date),
+      year: date.getFullYear(),
+    };
+  });
+}
+
+export default function CalendarPage() {
   return (
     <Main>
+      <WeekLinks />
+    </Main>
+  );
+}
+
+function WeekLinks() {
+  const data = useLoaderData<typeof loader>();
+
+  return (
+    <Flipper flipKey={data.weeks.map(({ number }) => number).join("")}>
       <div className="flex justify-center">
         <div className="calendar__weeks">
-          {weeks.map((week) => (
-            <div
-              key={week}
-              onClick={() => setWeeks(["Last", "This", "Next", "14", "15"])}
-              className="calendar__week"
-            >
-              <div>
-                {week} <br />
-                Week
-              </div>
-              <div className="calendar__event-count">×12</div>
-            </div>
+          {data.weeks.map((week) => (
+            <Flipped key={week.number} flipId={week.number}>
+              <Link
+                to={`?week=${week.number}&year=${week.year}`}
+                className="calendar__week"
+              >
+                <>
+                  <div>
+                    {week.number === data.thisWeek
+                      ? "This"
+                      : week.number - data.thisWeek === 1
+                      ? "Next"
+                      : week.number - data.thisWeek === -1
+                      ? "Last"
+                      : week.number}{" "}
+                    <br />
+                    Week
+                  </div>
+                  <div className="calendar__event-count">
+                    ×{week.numberOfEvents}
+                  </div>
+                </>
+              </Link>
+            </Flipped>
           ))}
         </div>
       </div>
-    </Main>
+    </Flipper>
   );
 }
--- app/utils/dates.ts
+++ app/utils/dates.ts
@@ -1,7 +1,28 @@
+import { getWeek } from "date-fns";
+
 export function databaseTimestampToDate(timestamp: number) {
   return new Date(timestamp * 1000);
 }
 
 export function dateToDatabaseTimestamp(date: Date) {
   return Math.floor(date.getTime() / 1000);
+}
+
+export function dateToWeekNumber(date: Date) {
+  return getWeek(date, { weekStartsOn: 1, firstWeekContainsDate: 4 });
+}
+
+// https://stackoverflow.com/a/71336659
+export function weekNumberToDate({
+  week,
+  year,
+}: {
+  week: number;
+  year: number;
+}) {
+  const result = new Date(year, 0, 4);
+  result.setDate(
+    result.getDate() - (result.getDay() || 7) + 1 + 7 * (week - 1)
+  );
+  return result;
 }
