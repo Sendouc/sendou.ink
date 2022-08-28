2722f7780294817b601ff56f8deed9e8d223313f
Kalle <38327916+Sendouc@users.noreply.github.com>
2022-08-28 18:10:57 +0300

    Add ability types

--- app/modules/in-game-lists/abilities.ts
+++ app/modules/in-game-lists/abilities.ts
@@ -1,28 +1,36 @@
-export const abilityCodes = [
-  "AD",
-  "BRU",
-  "CB",
-  "DR",
-  "H",
-  "IA",
-  "IRU",
-  "ISM",
-  "ISS",
-  "LDE",
-  "NS",
-  "OG",
-  "OS",
-  "QR",
-  "QSJ",
-  "RES",
-  "RP",
-  "RSU",
-  "SCU",
-  "SJ",
-  "SPU",
-  "SRU",
-  "SS",
-  "SSU",
-  "T",
-  "TI",
+export type AbilityType =
+  | "STACKABLE"
+  | "HEAD_ONLY"
+  | "CLOTHES_ONLY"
+  | "SHOES_ONLY";
+
+export const abilityCodes: Readonly<
+  Array<{ name: string; type: AbilityType }>
+> = [
+  { name: "AD", type: "CLOTHES_ONLY" },
+  { name: "BRU", type: "STACKABLE" },
+  { name: "CB", type: "HEAD_ONLY" },
+  { name: "DR", type: "SHOES_ONLY" },
+  { name: "H", type: "CLOTHES_ONLY" },
+  { name: "IA", type: "STACKABLE" },
+  { name: "IRU", type: "STACKABLE" },
+  { name: "ISM", type: "STACKABLE" },
+  { name: "ISS", type: "STACKABLE" },
+  { name: "LDE", type: "HEAD_ONLY" },
+  { name: "NS", type: "CLOTHES_ONLY" },
+  { name: "OG", type: "HEAD_ONLY" },
+  { name: "OS", type: "SHOES_ONLY" },
+  { name: "QR", type: "STACKABLE" },
+  { name: "QSJ", type: "STACKABLE" },
+  { name: "RES", type: "STACKABLE" },
+  { name: "RP", type: "CLOTHES_ONLY" },
+  { name: "RSU", type: "STACKABLE" },
+  { name: "SCU", type: "STACKABLE" },
+  { name: "SJ", type: "SHOES_ONLY" },
+  { name: "SPU", type: "STACKABLE" },
+  { name: "SRU", type: "STACKABLE" },
+  { name: "SS", type: "STACKABLE" },
+  { name: "SSU", type: "STACKABLE" },
+  { name: "T", type: "CLOTHES_ONLY" },
+  { name: "TI", type: "CLOTHES_ONLY" },
 ] as const;
