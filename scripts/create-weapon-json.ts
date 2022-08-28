98584b629922392e9c25b928121501a861b3bacb
Kalle <38327916+Sendouc@users.noreply.github.com>
2022-08-28 15:51:33 +0300

    Add weapon ids list

--- scripts/create-weapon-json.ts
+++ scripts/create-weapon-json.ts
@@ -5,7 +5,7 @@ import invariant from "tiny-invariant";
 
 const INTERNAL_NAMES_TO_IGNORE: readonly string[] = ["Free"] as const;
 const LANG_DICTS_PATH = path.join(__dirname, "dicts", "langs");
-const OUTPUT_PATH = path.join(__dirname, "output", "weapons.json");
+const OUTPUT_DIR_PATH = path.join(__dirname, "output");
 
 async function loadLangDicts() {
   const result: Array<
@@ -14,6 +14,8 @@ async function loadLangDicts() {
 
   const files = await fs.promises.readdir(LANG_DICTS_PATH);
   for (const file of files) {
+    if (file === ".gitkeep") continue;
+
     const translations = JSON.parse(
       fs.readFileSync(path.join(LANG_DICTS_PATH, file), "utf8")
     );
@@ -57,7 +59,16 @@ async function main() {
 
   result.sort((a, b) => a.id - b.id);
 
-  fs.writeFileSync(OUTPUT_PATH, JSON.stringify(result, null, 2));
+  const weaponIds = result.map((w) => w.id);
+
+  fs.writeFileSync(
+    path.join(OUTPUT_DIR_PATH, "weapons.json"),
+    JSON.stringify(result, null, 2)
+  );
+  fs.writeFileSync(
+    path.join(OUTPUT_DIR_PATH, "weapon-ids.json"),
+    JSON.stringify(weaponIds, null, 2)
+  );
 }
 
 void main();
