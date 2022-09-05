fe7a14168c0780056e01d5d5b45bfb15e6e8b43b
Kalle <38327916+Sendouc@users.noreply.github.com>
2022-09-05 21:12:20 +0300

    Gear and weapon translations for every language

--- app/modules/i18n/config.ts
+++ app/modules/i18n/config.ts
@@ -1,7 +1,50 @@
 export const DEFAULT_LANGUAGE = "en";
 
+export const languages = [
+  {
+    code: "de",
+    name: "Deutsch",
+  },
+  {
+    code: "en",
+    name: "English",
+  },
+  {
+    code: "es",
+    name: "Español",
+  },
+  {
+    code: "fr",
+    name: "Français",
+  },
+  {
+    code: "it",
+    name: "Italiano",
+  },
+  {
+    code: "ja",
+    name: "日本語",
+  },
+  {
+    code: "ko",
+    name: "한국어",
+  },
+  {
+    code: "nl",
+    name: "Nederlands",
+  },
+  {
+    code: "ru",
+    name: "Русский",
+  },
+  {
+    code: "zh",
+    name: "中文",
+  },
+];
+
 export const config = {
-  supportedLngs: ["en"],
+  supportedLngs: languages.map((lang) => lang.code),
   fallbackLng: DEFAULT_LANGUAGE,
   defaultNS: "common",
   react: { useSuspense: false },
