0b040e170ad1282673497bc7ba94525ec5215c59
Kalle <38327916+Sendouc@users.noreply.github.com>
2022-09-08 22:28:53 +0300

    Bake ability values calculation into abilityPointsToEffect

--- app/modules/analyzer/utils.ts
+++ app/modules/analyzer/utils.ts
@@ -1,3 +1,5 @@
+// xxx: rename file to something more accurate
+
 import type { BuildAbilitiesTupleWithUnknown } from "../in-game-lists";
 import weaponParamsJson from "./weapon-params.json";
 import abilityValuesJson from "./ability-values.json";
@@ -24,7 +26,7 @@ export function buildToAbilityPoints(build: BuildAbilitiesTupleWithUnknown) {
   return result;
 }
 
-export function abilityValues({
+function abilityValues({
   key,
   weapon,
 }: {
@@ -43,4 +45,47 @@ export function abilityValues({
     overwrites?.Mid ?? Mid,
     overwrites?.Low ?? Low,
   ];
+}
+
+function calculateAbilityPointToPercent(ap: number) {
+  return Math.min(3.3 * ap - 0.027 * Math.pow(ap, 2), 100);
+}
+
+function getSlope(high: number, mid: number, low: number) {
+  if (mid === low) {
+    return 0;
+  }
+  return (mid - low) / (high - low);
+}
+
+function lerpN(p: number, s: number) {
+  if (s.toFixed(3) === "0.500") {
+    return p;
+  }
+  if (p === 0.0) {
+    return p;
+  }
+  if (p === 1.0) {
+    return p;
+  }
+
+  return Math.pow(Math.E, -1 * ((Math.log(p) * Math.log(s)) / Math.log(2)));
+}
+
+export function abilityPointsToEffect({
+  key,
+  abilityPoints,
+  weapon,
+}: {
+  key: keyof typeof abilityValuesJson;
+  abilityPoints: number;
+  weapon: MainWeaponParams;
+}) {
+  const [high, mid, low] = abilityValues({ key, weapon });
+
+  const slope = getSlope(high, mid, low);
+  const percentage = calculateAbilityPointToPercent(abilityPoints) / 100.0;
+  const result = low + (high - low) * lerpN(slope, percentage);
+
+  return [result, lerpN(slope, percentage) * 100];
 }
