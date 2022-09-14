8827d633a32aa40718a16d039f28e60421ac2202
DoubleCookies <12849336+DoubleCookies@users.noreply.github.com>
2022-09-14 23:07:40 +0300

    move css to utils

--- app/routes/calendar/$id/report-winners.tsx
+++ app/routes/calendar/$id/report-winners.tsx
@@ -408,7 +408,7 @@ function Players({
                 variant="minimal"
                 onClick={() => handlePlayerInputTypeChange(i)}
                 data-cy="change-input-type-button"
-                className={"user-type-button"}
+                className={"outline-theme"}
               >
                 {asPlainInput
                   ? t("forms.team.player.addAsUser")
