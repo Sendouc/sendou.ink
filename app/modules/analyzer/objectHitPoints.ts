c918aafb9d3bb2433ed5f584afb0e8d2a92a7b37
Kalle <38327916+Sendouc@users.noreply.github.com>
2022-10-23 15:05:20 +0300

    Add remaining hitpoint values

--- app/modules/analyzer/objectHitPoints.ts
+++ app/modules/analyzer/objectHitPoints.ts
@@ -10,10 +10,13 @@ import type {
 import { hpDivided } from "./utils";
 import weaponParams from "./weapon-params.json";
 
-const PLACEHOLDER_HP = 1000;
 const WAVE_BREAKER_HP = 400;
 const SPRINKER_HP = 100;
 const RAINMAKER_HP = 1000;
+const SPLAT_BRELLA_SHIELD_HP = 500;
+const BOOYAH_BOMB_ARMOR_HP = 470;
+const BEAKON_HP = 120;
+const TORPEDO_HP = 20;
 
 export const objectHitPoints = (abilityPoints: AbilityPoints): HitPoints => {
   const Wsb_Shield = subStats({
@@ -38,7 +41,7 @@ export const objectHitPoints = (abilityPoints: AbilityPoints): HitPoints => {
   invariant(GreatBarrier_WeakPoint);
 
   return {
-    BulletUmbrellaCanopyNormal: PLACEHOLDER_HP, // ?? needs defaults
+    BulletUmbrellaCanopyNormal: SPLAT_BRELLA_SHIELD_HP,
     BulletUmbrellaCanopyWide: hpDivided(
       weaponParams.mainWeapons[6010].CanopyHP
     ),
@@ -46,14 +49,14 @@ export const objectHitPoints = (abilityPoints: AbilityPoints): HitPoints => {
       weaponParams.mainWeapons[6020].CanopyHP
     ),
     Wsb_Shield,
-    Bomb_TorpedoBullet: PLACEHOLDER_HP, // ??
+    Bomb_TorpedoBullet: TORPEDO_HP,
     Chariot: hpDivided(weaponParams.specialWeapons[CRAB_TANK_ID].ArmorHP),
     Gachihoko_Barrier: RAINMAKER_HP,
     GreatBarrier_Barrier,
     GreatBarrier_WeakPoint,
-    NiceBall_Armor: PLACEHOLDER_HP, // ??
+    NiceBall_Armor: BOOYAH_BOMB_ARMOR_HP, // ??
     ShockSonar: WAVE_BREAKER_HP,
-    Wsb_Flag: PLACEHOLDER_HP, // ??
+    Wsb_Flag: BEAKON_HP,
     Wsb_Sprinkler: SPRINKER_HP,
   };
 };
