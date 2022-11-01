0d8b88ea5a01209a032e324b1bcf02de8353e766
William Lam <w.lam@honteam.com>
2022-11-01 02:57:44 -0400

    All articles page (#1072)
    * Added new Article route that renders all Articles (with a reasonable upper limit for now).
    - Also added a GoToPageBanner for the main Articles page (/a) on the front page. This is placed above the ArticlesPeek component
    - Used the sendou_love navItem icon for the moment (this can be replaced at any time once a new art asset is added)
    
    * Fixed Prettier CI pipeline error
    
    * Ran `npm run check-translation-jsons` to update missing Translations for new key added
    
    * Update public/locales/en/front.json
    
    Co-authored-by: Kalle <38327916+Sendouc@users.noreply.github.com>
    
    * French localizations
    
    * Removed unnecessary comment
    
    * Converted h2 to h1 tags for Main Articles page
    
    Co-authored-by: Kalle <38327916+Sendouc@users.noreply.github.com>
    
    * Moved GoToPageBanner element for main articles page to below ArticlesPeek element
    
    * Refactored some files so that the articles prop is passed into ArticlesPeek
    - Also added the Articles header title in common.json
    
    * Updated translation-progress.md
    
    * Cleaned up data type for articles prop in ArticlesPeek
    
    * Fixed React hydration error for translations on imported articles
    - Added in SendouRouteHandle component on new Articles main page
    
    * GoToPageBanner now optionally accepts a `navItem` parameter. It does not render the image if navItem is not passed into it
    
    * Removed <Outlet /> JSX element on the main Articles page
    
    * Removed sendou_love navItem on GoToPageBanner for articles page
    
    * Fixed spacing issue by grouping ArticlesPeek and its corresponding GoToPageBanner element into a `stack` div
    
    * Fixed pipeline error
    
    * Update translation again
    
    Co-authored-by: Kalle <38327916+Sendouc@users.noreply.github.com>

---
+++ app/routes/a.tsx
@@ -0,0 +1,38 @@
+import { useLoaderData } from "@remix-run/react";
+import { Main } from "~/components/Main";
+import type { LinksFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { mostRecentArticles } from "~/modules/articles";
+import styles from "~/styles/front.css";
+import { ArticlesPeek } from ".";
+import { useTranslation } from "react-i18next";
+import type { SendouRouteHandle } from "~/utils/remix";
+
+const MAX_ARTICLES_COUNT = 100;
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: styles }];
+};
+
+export const handle: SendouRouteHandle = {
+  i18n: ["front"],
+};
+
+export const loader = async () => {
+  return json({
+    recentArticles: await mostRecentArticles(MAX_ARTICLES_COUNT),
+  });
+};
+
+export default function ArticlesMainPage() {
+  const { t } = useTranslation("common");
+  const data = useLoaderData<typeof loader>();
+  const articles = data.recentArticles;
+
+  return (
+    <Main className="stack lg">
+      <h1>{t("pages.articles")}</h1>
+      <ArticlesPeek articles={articles} />
+    </Main>
+  );
+}
