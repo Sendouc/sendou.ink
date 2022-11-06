70a90b724e68668eb6b52a97b71e0bb8fd99acae
Kalle <38327916+Sendouc@users.noreply.github.com>
2022-11-06 11:48:07 +0200

    Add tournament map list generation tests

--- app/modules/tournament-map-list-generator/generation.test.ts
+++ app/modules/tournament-map-list-generator/generation.test.ts
@@ -7,6 +7,33 @@ import type { TournamentMaplistInput } from "./types";
 
 const TournamentMapListGenerator = suite("Tournament map list generator");
 
+const team1Picks = new MapPool([
+  { mode: "SZ", stageId: 4 },
+  { mode: "SZ", stageId: 5 },
+  { mode: "TC", stageId: 5 },
+  { mode: "TC", stageId: 6 },
+  { mode: "RM", stageId: 7 },
+  { mode: "RM", stageId: 8 },
+  { mode: "CB", stageId: 9 },
+  { mode: "CB", stageId: 10 },
+]);
+const team2Picks = new MapPool([
+  { mode: "SZ", stageId: 11 },
+  { mode: "SZ", stageId: 9 },
+  { mode: "TC", stageId: 2 },
+  { mode: "TC", stageId: 8 },
+  { mode: "RM", stageId: 7 },
+  { mode: "RM", stageId: 1 },
+  { mode: "CB", stageId: 2 },
+  { mode: "CB", stageId: 3 },
+]);
+const tiebreaker = new MapPool([
+  { mode: "SZ", stageId: 1 },
+  { mode: "TC", stageId: 2 },
+  { mode: "RM", stageId: 3 },
+  { mode: "CB", stageId: 4 },
+]);
+
 const generateMaps = ({
   bestOf = 5,
   bracketType = "SE",
@@ -14,37 +41,14 @@ const generateMaps = ({
   teams = [
     {
       name: "Team 1",
-      maps: new MapPool([
-        { mode: "SZ", stageId: 4 },
-        { mode: "SZ", stageId: 5 },
-        { mode: "TC", stageId: 5 },
-        { mode: "TC", stageId: 6 },
-        { mode: "RM", stageId: 7 },
-        { mode: "RM", stageId: 8 },
-        { mode: "CB", stageId: 9 },
-        { mode: "CB", stageId: 10 },
-      ]),
+      maps: team1Picks,
     },
     {
       name: "Team 2",
-      maps: new MapPool([
-        { mode: "SZ", stageId: 11 },
-        { mode: "SZ", stageId: 9 },
-        { mode: "TC", stageId: 2 },
-        { mode: "TC", stageId: 8 },
-        { mode: "RM", stageId: 7 },
-        { mode: "RM", stageId: 1 },
-        { mode: "CB", stageId: 2 },
-        { mode: "CB", stageId: 3 },
-      ]),
+      maps: team2Picks,
     },
   ],
-  tiebreakerMaps = new MapPool([
-    { mode: "SZ", stageId: 1 },
-    { mode: "TC", stageId: 2 },
-    { mode: "RM", stageId: 3 },
-    { mode: "CB", stageId: 4 },
-  ]),
+  tiebreakerMaps = tiebreaker,
 }: Partial<TournamentMaplistInput>) => {
   return createTournamentMapList({
     bestOf,
@@ -54,15 +58,6 @@ const generateMaps = ({
     tiebreakerMaps,
   });
 };
-
-// generates map list with equal picks (our, their, our, their, tiebreaker)
-// no stage repeats in optimal case
-// always generates same map list given same input
-// order of team doesn't matter
-// worst case with duplication (identical maplists and overlap with tiebreaker)
-// if some overlap in maplist, keeps it fair (same amount of "picked" maps)
-// if no maplist from other team then pick maps from the team who submitted
-// makes maplist even if neither team submitted maps
 
 TournamentMapListGenerator("Modes are spread evenly", () => {
   const mapList = generateMaps({});
@@ -78,6 +73,214 @@ TournamentMapListGenerator("Modes are spread evenly", () => {
 
     modes.delete(mode);
   }
+});
+
+TournamentMapListGenerator("Equal picks", () => {
+  let our = 0;
+  let their = 0;
+  let tiebreaker = 0;
+
+  const mapList = generateMaps({});
+
+  for (const { stageId, mode } of mapList) {
+    if (team1Picks.has({ stageId, mode })) {
+      our++;
+    } else if (team2Picks.has({ stageId, mode })) {
+      their++;
+    } else {
+      tiebreaker++;
+    }
+  }
+
+  assert.equal(our, 2);
+  assert.equal(their, 2);
+  assert.equal(tiebreaker, 1);
+});
+
+TournamentMapListGenerator("No stage repeats in optimal case", () => {
+  const mapList = generateMaps({});
+
+  const stages = new Set(mapList.map(({ stageId }) => stageId));
+
+  assert.equal(stages.size, 5);
+});
+
+TournamentMapListGenerator(
+  "Always generates same maplist given same input",
+  () => {
+    const mapList1 = generateMaps({});
+    const mapList2 = generateMaps({});
+
+    assert.equal(mapList1.length, 5);
+
+    for (let i = 0; i < mapList1.length; i++) {
+      assert.equal(mapList1[i]!.stageId, mapList2[i]!.stageId);
+      assert.equal(mapList1[i]!.mode, mapList2[i]!.mode);
+    }
+  }
+);
+
+TournamentMapListGenerator(
+  "Order of team doesn't matter regarding what maplist gets created",
+  () => {
+    const mapList1 = generateMaps({});
+    const mapList2 = generateMaps({
+      teams: [
+        {
+          name: "Team 2",
+          maps: team2Picks,
+        },
+        {
+          name: "Team 1",
+          maps: team1Picks,
+        },
+      ],
+    });
+
+    assert.equal(mapList1.length, 5);
+
+    for (let i = 0; i < mapList1.length; i++) {
+      assert.equal(mapList1[i]!.stageId, mapList2[i]!.stageId);
+      assert.equal(mapList1[i]!.mode, mapList2[i]!.mode);
+    }
+  }
+);
+
+const duplicationPicks = new MapPool([
+  { mode: "SZ", stageId: 4 },
+  { mode: "SZ", stageId: 5 },
+  { mode: "TC", stageId: 4 },
+  { mode: "TC", stageId: 5 },
+  { mode: "RM", stageId: 6 },
+  { mode: "RM", stageId: 7 },
+  { mode: "CB", stageId: 6 },
+  { mode: "CB", stageId: 7 },
+]);
+const duplicationTiebreaker = new MapPool([
+  { mode: "SZ", stageId: 7 },
+  { mode: "TC", stageId: 6 },
+  { mode: "RM", stageId: 5 },
+  { mode: "CB", stageId: 4 },
+]);
+
+TournamentMapListGenerator(
+  "Uses other teams maps if one didn't submit maplist",
+  () => {
+    const mapList = generateMaps({
+      teams: [
+        {
+          name: "Team 1",
+          maps: new MapPool([]),
+        },
+        {
+          name: "Team 2",
+          maps: team2Picks,
+        },
+      ],
+    });
+
+    assert.equal(mapList.length, 5);
+
+    for (let i = 0; i < mapList.length - 1; i++) {
+      // map belongs to team 2 map list
+      const map = mapList[i];
+      assert.ok(map);
+
+      team2Picks.has({ mode: map.mode, stageId: map.stageId });
+    }
+  }
+);
+
+TournamentMapListGenerator(
+  "Creates map list even if neither team submitted maps",
+  () => {
+    const mapList = generateMaps({
+      teams: [
+        {
+          name: "Team 1",
+          maps: new MapPool([]),
+        },
+        {
+          name: "Team 2",
+          maps: new MapPool([]),
+        },
+      ],
+    });
+
+    assert.equal(mapList.length, 5);
+  }
+);
+
+TournamentMapListGenerator("Handles worst case with duplication", () => {
+  const maplist = generateMaps({
+    teams: [
+      {
+        name: "Team 1",
+        maps: duplicationPicks,
+      },
+      {
+        name: "Team 2",
+        maps: duplicationPicks,
+      },
+    ],
+    bestOf: 7,
+    tiebreakerMaps: duplicationTiebreaker,
+  });
+
+  assert.equal(maplist.length, 7);
+
+  // all stages appear
+  const stages = new Set(maplist.map(({ stageId }) => stageId));
+  assert.equal(stages.size, 4);
+
+  // no consecutive stage replays
+  for (let i = 0; i < maplist.length - 1; i++) {
+    assert.not.equal(maplist[i]!.stageId, maplist[i + 1]!.stageId);
+  }
+});
+
+const team2PicksWithSomeDuplication = new MapPool([
+  { mode: "SZ", stageId: 4 },
+  { mode: "SZ", stageId: 11 },
+  { mode: "TC", stageId: 5 },
+  { mode: "TC", stageId: 6 },
+  { mode: "RM", stageId: 7 },
+  { mode: "RM", stageId: 2 },
+  { mode: "CB", stageId: 9 },
+  { mode: "CB", stageId: 10 },
+]);
+
+TournamentMapListGenerator("Keeps things fair when overlap", () => {
+  const mapList = generateMaps({
+    teams: [
+      {
+        name: "Team 1",
+        maps: team1Picks,
+      },
+      {
+        name: "Team 2",
+        maps: team2PicksWithSomeDuplication,
+      },
+    ],
+    bestOf: 7,
+  });
+
+  assert.equal(mapList.length, 7);
+
+  let team1PicksAppeared = 0;
+  let team2PicksAppeared = 0;
+
+  for (const { stageId, mode } of mapList) {
+    if (team1Picks.has({ stageId, mode })) {
+      team1PicksAppeared++;
+    }
+
+    if (team2Picks.has({ stageId, mode })) {
+      team2PicksAppeared++;
+    }
+  }
+
+  assert.equal(team1PicksAppeared, team2PicksAppeared);
 });
 
 TournamentMapListGenerator.run();
