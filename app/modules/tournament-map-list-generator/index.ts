75aaf8fbf64f8337ccee78521ca0bf9225e15da4
Kalle <38327916+Sendouc@users.noreply.github.com>
2022-11-06 18:13:32 +0200

    Generation with strats initial

--- app/modules/tournament-map-list-generator/index.ts
+++ app/modules/tournament-map-list-generator/index.ts
@@ -1,1 +1,4 @@
 export { createTournamentMapList } from "./tournament-map-list";
+
+// 1. normal strat, each tiebreaker map one by one till finds one fitting
+// x. just generate some map list from "good maps"
--- app/modules/tournament-map-list-generator/tournament-map-list.ts
+++ app/modules/tournament-map-list-generator/tournament-map-list.ts
@@ -5,32 +5,108 @@ import type { TournamentMaplistInput } from "./types";
 import { seededRandom } from "./utils";
 
 export function createTournamentMapList({
+  bracketType,
+  roundNumber,
+  bestOf,
+  ...rest
+}: TournamentMaplistInput) {
+  const modesOrder = seededShuffle({
+    bracketType,
+    roundNumber,
+    seedId: "mode",
+  })(rankedModesShort);
+
+  // startingMode will be the mode used in tiebreaker
+  for (const startingMode of modesOrder) {
+    const list = equalNoStageRepeatStrategy({
+      ...rest,
+      bestOf,
+      bracketType,
+      roundNumber,
+      modesOrder: resolveModesOrder({
+        bracketType,
+        roundNumber,
+        startingMode,
+        bestOf,
+      }),
+    });
+
+    if (list) return list;
+  }
+
+  // strat that allows duplicate maps (map on both teams list)
+
+  throw new Error("not implemented");
+}
+
+function seededShuffle({
+  bracketType,
+  roundNumber,
+  seedId,
+}: Pick<TournamentMaplistInput, "bracketType" | "roundNumber"> & {
+  seedId: string;
+}) {
+  const { shuffle } = seededRandom(`${bracketType}-${roundNumber}-${seedId}`);
+
+  return shuffle;
+}
+
+function resolveModesOrder({
+  startingMode,
+  bracketType,
+  roundNumber,
+  bestOf,
+}: {
+  startingMode: ModeShort;
+  bracketType: TournamentMaplistInput["bracketType"];
+  roundNumber: TournamentMaplistInput["roundNumber"];
+  bestOf: TournamentMaplistInput["bestOf"];
+}) {
+  const result = [startingMode];
+
+  const modes = seededShuffle({
+    bracketType,
+    roundNumber,
+    seedId: startingMode,
+  })(rankedModesShort.filter((mode) => mode !== startingMode));
+  modes.push(startingMode);
+
+  while (result.length < bestOf) {
+    const nextMode = modes.shift()!;
+    result.push(nextMode);
+    modes.push(nextMode);
+  }
+
+  return result.reverse();
+}
+
+function equalNoStageRepeatStrategy({
   teams,
   bestOf,
   tiebreakerMaps,
   bracketType,
   roundNumber,
-}: TournamentMaplistInput) {
+  modesOrder,
+}: TournamentMaplistInput & { modesOrder: ModeShort[] }) {
   const result: ModeWithStage[] = [];
 
-  const seededShuffle = (seedPart: string) => {
-    const { shuffle } = seededRandom(
-      `${bracketType}-${roundNumber}-${seedPart}`
-    );
-
-    return shuffle;
-  };
-
-  const stagesUsed = new Set<StageId>();
-  const modesOrder = seededShuffle("modes")(rankedModesShort);
+  // let's already mark tiebreaker stage as used so we don't use it in regular maps and "fail the run"
+  const tiebreakerStageId = tiebreakerMaps.stageModePairs.find(
+    (pair) => pair.mode === modesOrder[modesOrder.length - 1]
+  )!.stageId;
+  const stagesUsed = new Set<StageId>([tiebreakerStageId]);
 
   const teamsSortedByName = teams.sort((a, b) => a.name.localeCompare(b.name));
-  const teamOneMaps = seededShuffle("teamOne")(
-    teamsSortedByName[0].maps.stageModePairs
-  );
-  const teamTwoMaps = seededShuffle("teamTwo")(
-    teamsSortedByName[1].maps.stageModePairs
-  );
+  const teamOneMaps = seededShuffle({
+    bracketType,
+    roundNumber,
+    seedId: "teamOne",
+  })(teamsSortedByName[0].maps.stageModePairs);
+  const teamTwoMaps = seededShuffle({
+    bracketType,
+    roundNumber,
+    seedId: "teamTwo",
+  })(teamsSortedByName[1].maps.stageModePairs);
 
   for (let i = 0; i < bestOf; i++) {
     const modeOfThisRound = modesOrder.shift()!;
@@ -48,7 +124,7 @@ export function createTournamentMapList({
 
     let found = false;
 
-    for (const { mode, stageId } of resolveMapList({
+    for (const { mode, stageId } of resolveTeamMapPoolToUse({
       index: i,
       teamOneMaps,
       teamTwoMaps,
@@ -62,14 +138,18 @@ export function createTournamentMapList({
     }
 
     if (!found) {
-      throw new Error("No map found from team map list");
+      return null;
     }
+  }
+
+  if (isUnfair({ maybeResult: result, teams })) {
+    return null;
   }
 
   return result;
 }
 
-function resolveMapList({
+function resolveTeamMapPoolToUse({
   teamOneMaps,
   teamTwoMaps,
   index,
@@ -82,4 +162,22 @@ function resolveMapList({
   if (teamTwoMaps.length === 0) return teamOneMaps;
 
   return index % 2 === 0 ? teamOneMaps : teamTwoMaps;
+}
+
+function isUnfair({
+  maybeResult,
+  teams,
+}: {
+  maybeResult: ModeWithStage[];
+  teams: TournamentMaplistInput["teams"];
+}) {
+  let teamOneCount = 0;
+  let teamTwoCount = 0;
+
+  for (const { mode, stageId } of maybeResult) {
+    if (teams[0].maps.has({ mode, stageId })) teamOneCount++;
+    if (teams[1].maps.has({ mode, stageId })) teamTwoCount++;
+  }
+
+  return teamOneCount !== teamTwoCount;
 }
