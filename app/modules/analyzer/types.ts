7055d662463d8720386722393c10ce8044d013e8
William Lam <w.lam@honteam.com>
2022-11-11 16:59:48 -0500

    Fixed typing error

--- app/modules/analyzer/types.ts
+++ app/modules/analyzer/types.ts
@@ -8,7 +8,7 @@ import type { SPECIAL_EFFECTS } from "./specialEffects";
 import type weaponParams from "./weapon-params.json";
 import type abilityValues from "./ability-values.json";
 import type { DAMAGE_RECEIVERS, DAMAGE_TYPE } from "./constants";
-import { AbilityWithUnknown } from "../in-game-lists/types";
+import type { AbilityWithUnknown } from "../in-game-lists/types";
 
 type Overwrites = Record<
   string,
