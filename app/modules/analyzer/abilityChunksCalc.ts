863043f775dead77c5a726ab52787a67cbb44ab4
William Lam <w.lam@honteam.com>
2022-11-12 11:17:14 -0500

    Refactored code to process mainAbilities and subAbilities

--- app/modules/analyzer/abilityChunksCalc.ts
+++ app/modules/analyzer/abilityChunksCalc.ts
@@ -7,6 +7,7 @@ import { abilities } from "../in-game-lists";
 import {
   PRIMARY_SLOT_ONLY_REQUIRED_ABILITY_CHUNKS_COUNT,
   REQUIRED_ABILITY_CHUNKS_COUNT,
+  SUB_REQUIRED_ABILITY_CHUNKS_COUNT,
 } from "../in-game-lists/abilities";
 import type {
   AbilityWithUnknown,
@@ -31,34 +32,81 @@ export function getAbilityChunksMapAsArray(
     })
   );
 
-  for (let i = 0; i < mainAbilities.length; i++) {
-    const mainAbility = mainAbilities[i];
+  updateAbilityChunksMap(abilityChunksMap, mainAbilities, true);
+  updateAbilityChunksMap(abilityChunksMap, subAbilities, false);
 
-    if (!mainAbility) continue;
+  return Array.from(abilityChunksMap).sort((a, b) => b[1] - a[1]);
+}
 
-    const primarySlotOnlyAbilityRef = abilities.filter(
-      (a) => a.name === mainAbility && a.abilityChunkTypesRequired.length > 0
-    );
+function updateAbilityChunksMap(
+  abilityChunksMap: AbilityChunks,
+  abilityList: (
+    | "ISM"
+    | "ISS"
+    | "IRU"
+    | "RSU"
+    | "SSU"
+    | "SCU"
+    | "SS"
+    | "SPU"
+    | "QR"
+    | "QSJ"
+    | "BRU"
+    | "RES"
+    | "SRU"
+    | "IA"
+    | "OG"
+    | "LDE"
+    | "T"
+    | "CB"
+    | "NS"
+    | "H"
+    | "TI"
+    | "RP"
+    | "AD"
+    | "SJ"
+    | "OS"
+    | "DR"
+  )[],
+  isInMainAbilitySlot: boolean
+) {
+  for (const selectedAbility of abilityList) {
+    if (!selectedAbility) continue;
 
-    // Primary-slot-only item that can have ability chunks put on it
-    if (primarySlotOnlyAbilityRef.length === 1) {
-      const primaryAbility = primarySlotOnlyAbilityRef[0];
-      if (!primaryAbility) continue;
+    if (isInMainAbilitySlot) {
+      const primarySlotOnlyAbilityRef = abilities.filter(
+        (a) =>
+          a.name === selectedAbility && a.abilityChunkTypesRequired.length > 0
+      );
 
-      for (const ability of primaryAbility.abilityChunkTypesRequired) {
+      // Primary-slot-only item that can have ability chunks put on it
+      if (primarySlotOnlyAbilityRef.length === 1) {
+        const primaryAbility = primarySlotOnlyAbilityRef[0];
+        if (!primaryAbility) continue;
+
+        for (const ability of primaryAbility.abilityChunkTypesRequired) {
+          abilityChunksMap.set(
+            ability,
+            (abilityChunksMap.get(ability) ?? 0) +
+              PRIMARY_SLOT_ONLY_REQUIRED_ABILITY_CHUNKS_COUNT
+          );
+        }
+      } else {
         abilityChunksMap.set(
-          ability,
-          (abilityChunksMap.get(ability) ?? 0) +
-            PRIMARY_SLOT_ONLY_REQUIRED_ABILITY_CHUNKS_COUNT
+          selectedAbility,
+          (abilityChunksMap.get(selectedAbility) ?? 0) +
+            REQUIRED_ABILITY_CHUNKS_COUNT
         );
       }
-    } else {
+    }
+
+    // Ability is in a sub slot
+    else {
       abilityChunksMap.set(
-        mainAbility,
-        (abilityChunksMap.get(mainAbility) ?? 0) + REQUIRED_ABILITY_CHUNKS_COUNT
+        selectedAbility,
+        (abilityChunksMap.get(selectedAbility) ?? 0) +
+          SUB_REQUIRED_ABILITY_CHUNKS_COUNT
       );
     }
   }
-
-  return Array.from(abilityChunksMap).sort((a, b) => b[1] - a[1]);
 }
