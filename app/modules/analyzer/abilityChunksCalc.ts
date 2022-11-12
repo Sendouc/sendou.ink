c636886c266ff09ce8b3c23f09cf84d780981eef
William Lam <w.lam@honteam.com>
2022-11-12 10:18:00 -0500

    Moved logical function to a new file in the analyzer module called abilityChunksCalc.ts - Refactored for loop content to be cleaner - Removed & changed some comments

---
+++ app/modules/analyzer/abilityChunksCalc.ts
@@ -0,0 +1,53 @@
+// From an array of Main abilities, create a map of <Ability, number>, then return it as an Array after sorting by value, descending.
+//    The map describes the number of Ability chunks required to replace the main ability on a piece of gear.
+// Extra processing is required for Main abilities that are primary slot-only abilities,
+//    as they are comprised of 3 stackable ability chunks at a lower ability chunk count than usual.
+
+import { abilities } from "../in-game-lists";
+import {
+  PRIMARY_SLOT_ONLY_REQUIRED_ABILITY_CHUNKS_COUNT,
+  REQUIRED_ABILITY_CHUNKS_COUNT,
+} from "../in-game-lists/abilities";
+import type { AbilityWithUnknown } from "../in-game-lists/types";
+import type { MainAbilityChunks } from "./types";
+
+export function getAbilityChunksMapAsArray(
+  mainAbilities: AbilityWithUnknown[]
+) {
+  const abilityChunksMap: MainAbilityChunks = new Map<
+    AbilityWithUnknown,
+    number
+  >();
+  for (let i = 0; i < mainAbilities.length; i++) {
+    const mainAbility = mainAbilities[i];
+    if (typeof mainAbility !== "undefined" && mainAbility !== "UNKNOWN") {
+      const primarySlotOnlyAbilityRef = abilities.filter(
+        (a) => a.name === mainAbility && a.abilityChunkTypesRequired.length > 0
+      );
+
+      // Primary-slot-only item that can have ability chunks put on it
+      if (primarySlotOnlyAbilityRef.length === 1) {
+        const primaryAbility = primarySlotOnlyAbilityRef[0];
+        if (!primaryAbility) continue;
+
+        for (const ability of primaryAbility.abilityChunkTypesRequired) {
+          if (typeof ability !== "undefined") {
+            abilityChunksMap.set(
+              ability,
+              (abilityChunksMap.get(ability) ?? 0) +
+                PRIMARY_SLOT_ONLY_REQUIRED_ABILITY_CHUNKS_COUNT
+            );
+          }
+        }
+      } else {
+        abilityChunksMap.set(
+          mainAbility,
+          (abilityChunksMap.get(mainAbility) ?? 0) +
+            REQUIRED_ABILITY_CHUNKS_COUNT
+        );
+      }
+    }
+  }
+
+  return Array.from(abilityChunksMap).sort((a, b) => b[1] - a[1]);
+}
--- app/routes/analyzer.tsx
+++ app/routes/analyzer.tsx
@@ -19,7 +19,6 @@ import {
 } from "~/modules/analyzer/specialEffects";
 import type {
   AbilityPoints,
-  MainAbilityChunks,
   SpecialEffectType,
 } from "~/modules/analyzer/types";
 import {
@@ -48,10 +47,7 @@ import {
   subWeaponImageUrl,
 } from "~/utils/urls";
 import type { AbilityWithUnknown } from "~/modules/in-game-lists/types";
-import {
-  PRIMARY_SLOT_ONLY_REQUIRED_ABILITY_CHUNKS_COUNT,
-  REQUIRED_ABILITY_CHUNKS_COUNT,
-} from "~/modules/in-game-lists/abilities";
+import { getAbilityChunksMapAsArray } from "~/modules/analyzer/abilityChunksCalc";
 
 export const CURRENT_PATCH = "1.2";
 
@@ -749,57 +745,6 @@ function AbilityPointsDetails({
       </div>
     </details>
   );
-}
-
-/**
- * From an array of Main abilities, create a map of <Ability, number>, then return it as an Array after sorting by value, descending.
- *
- * The map describes the number of Ability chunks required to replace the main ability on a piece of gear.
- *
- * Extra processing is required for Main abilities that are primary slot-only abilities,
- *  as they are comprised of 3 stackable ability chunks at a lower ability chunk count than usual.
- */
-function getAbilityChunksMapAsArray(mainAbilities: AbilityWithUnknown[]) {
-  const abilityChunksMap: MainAbilityChunks = new Map();
-  for (let i = 0; i < mainAbilities.length; i++) {
-    const mainAbility = mainAbilities[i];
-    if (typeof mainAbility !== "undefined" && mainAbility !== "UNKNOWN") {
-      const primarySlotOnlyAbilityRef = abilities.filter(
-        (a) => a.name === mainAbility && a.abilityChunkTypesRequired.length > 0
-      );
-
-      // Primary-slot-only item that can have ability chunks put on it
-      if (primarySlotOnlyAbilityRef.length === 1) {
-        const primaryAbility = primarySlotOnlyAbilityRef[0];
-        if (typeof primaryAbility !== "undefined") {
-          for (
-            let j = 0;
-            j < primaryAbility?.abilityChunkTypesRequired.length;
-            j++
-          ) {
-            const mainAbilityName = primaryAbility.abilityChunkTypesRequired[j];
-
-            if (typeof mainAbilityName !== "undefined") {
-              abilityChunksMap.set(
-                mainAbilityName,
-                (abilityChunksMap.get(mainAbility) ?? 0) +
-                  PRIMARY_SLOT_ONLY_REQUIRED_ABILITY_CHUNKS_COUNT
-              );
-            }
-          }
-        }
-      } else {
-        abilityChunksMap.set(
-          mainAbility,
-          (abilityChunksMap.get(mainAbility) ?? 0) +
-            REQUIRED_ABILITY_CHUNKS_COUNT
-        );
-      }
-    }
-  }
-
-  // After building the map, convert it to an array & sort the map by value, descending
-  return Array.from(abilityChunksMap).sort((a, b) => b[1] - a[1]);
 }
 
 function AbilityChunksRequired({
