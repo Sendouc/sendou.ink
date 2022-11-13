7547a27aa261c0d2895e893e71aa49cd1f6f2445
William Lam <w.lam@honteam.com>
2022-11-13 10:29:53 -0500

    Fixed Prettier error

--- app/modules/analyzer/abilityChunksCalc.test.ts
+++ app/modules/analyzer/abilityChunksCalc.test.ts
@@ -43,61 +43,70 @@ GetAbilityChunksMapAsArray("Empty build results in an empty array", () => {
   assert.equal(abilityChunksArray, []);
 });
 
-GetAbilityChunksMapAsArray("Main Ability stackable ability chunk calculation is correct", () => {
-  const build = [
-    ["ISS", "UNKNOWN", "UNKNOWN", "UNKNOWN"],
-    ["ISM", "UNKNOWN", "UNKNOWN", "UNKNOWN"],
-    ["UNKNOWN", "UNKNOWN", "UNKNOWN", "UNKNOWN"],
-  ] as unknown as BuildAbilitiesTupleWithUnknown;
+GetAbilityChunksMapAsArray(
+  "Main Ability stackable ability chunk calculation is correct",
+  () => {
+    const build = [
+      ["ISS", "UNKNOWN", "UNKNOWN", "UNKNOWN"],
+      ["ISM", "UNKNOWN", "UNKNOWN", "UNKNOWN"],
+      ["UNKNOWN", "UNKNOWN", "UNKNOWN", "UNKNOWN"],
+    ] as unknown as BuildAbilitiesTupleWithUnknown;
 
-  const expectedOutput = [
-    ["ISM", 45],
-    ["ISS", 45],
-  ];
+    const expectedOutput = [
+      ["ISM", 45],
+      ["ISS", 45],
+    ];
 
-  const abilityChunksArray = getAbilityChunksMapAsArray(build);
-  validateAbilityChunksArray(abilityChunksArray, expectedOutput);
-});
+    const abilityChunksArray = getAbilityChunksMapAsArray(build);
+    validateAbilityChunksArray(abilityChunksArray, expectedOutput);
+  }
+);
 
-GetAbilityChunksMapAsArray("Ninja Squid ability chunk calculation is correct", () => {
-  const build = [
-    ["UNKNOWN", "UNKNOWN", "UNKNOWN", "UNKNOWN"],
-    ["NS", "UNKNOWN", "UNKNOWN", "UNKNOWN"],
-    ["UNKNOWN", "UNKNOWN", "UNKNOWN", "UNKNOWN"],
-  ] as unknown as BuildAbilitiesTupleWithUnknown;
+GetAbilityChunksMapAsArray(
+  "Ninja Squid ability chunk calculation is correct",
+  () => {
+    const build = [
+      ["UNKNOWN", "UNKNOWN", "UNKNOWN", "UNKNOWN"],
+      ["NS", "UNKNOWN", "UNKNOWN", "UNKNOWN"],
+      ["UNKNOWN", "UNKNOWN", "UNKNOWN", "UNKNOWN"],
+    ] as unknown as BuildAbilitiesTupleWithUnknown;
 
-  const expectedOutput = [
-    ["RSU", 15],
-    ["IRU", 15],
-    ["SSU", 15],
-  ];
+    const expectedOutput = [
+      ["RSU", 15],
+      ["IRU", 15],
+      ["SSU", 15],
+    ];
 
-  const abilityChunksArray = getAbilityChunksMapAsArray(build);
-  validateAbilityChunksArray(abilityChunksArray, expectedOutput);
-});
+    const abilityChunksArray = getAbilityChunksMapAsArray(build);
+    validateAbilityChunksArray(abilityChunksArray, expectedOutput);
+  }
+);
 
-GetAbilityChunksMapAsArray("Ability chunk calculation is correct for a real build. Each gear has 1, 2 or 3 ability chunks of same type", () => {
-  const slayerBuild = [
-    ["LDE", "SSU", "SSU", "SSU"],
-    ["NS", "QR", "QR", "ISM"],
-    ["SJ", "SSU", "RES", "QSJ"],
-  ] as unknown as BuildAbilitiesTupleWithUnknown;
+GetAbilityChunksMapAsArray(
+  "Ability chunk calculation is correct for a real build. Each gear has 1, 2 or 3 ability chunks of same type",
+  () => {
+    const slayerBuild = [
+      ["LDE", "SSU", "SSU", "SSU"],
+      ["NS", "QR", "QR", "ISM"],
+      ["SJ", "SSU", "RES", "QSJ"],
+    ] as unknown as BuildAbilitiesTupleWithUnknown;
 
-  const expectedOutput = [
-    ["SSU", 85],
-    ["IRU", 30],
-    ["QR", 30],
-    ["ISM", 25],
-    ["QSJ", 25],    
-    ["IA", 15],
-    ["ISS", 15],
-    ["RSU", 15],
-    ["SRU", 15],
-    ["RES", 10],
-  ];
+    const expectedOutput = [
+      ["SSU", 85],
+      ["IRU", 30],
+      ["QR", 30],
+      ["ISM", 25],
+      ["QSJ", 25],
+      ["IA", 15],
+      ["ISS", 15],
+      ["RSU", 15],
+      ["SRU", 15],
+      ["RES", 10],
+    ];
 
-  const abilityChunksArray = getAbilityChunksMapAsArray(slayerBuild);
-  validateAbilityChunksArray(abilityChunksArray, expectedOutput);
-});
+    const abilityChunksArray = getAbilityChunksMapAsArray(slayerBuild);
+    validateAbilityChunksArray(abilityChunksArray, expectedOutput);
+  }
+);
 
 GetAbilityChunksMapAsArray.run();
