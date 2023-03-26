33498e211e91603376ff30e4c70074988b242b95
Kalle <38327916+Sendouc@users.noreply.github.com>
2023-03-26 19:42:18 +0300

    Add badge winners script

---
+++ scripts/add-badge-winners.ts
@@ -0,0 +1,26 @@
+/* eslint-disable no-console */
+import "dotenv/config";
+import invariant from "tiny-invariant";
+import { sql } from "~/db/sql";
+
+const badgeId = process.argv[2]?.trim();
+const discordIds = process.argv[3]?.trim();
+
+invariant(discordIds, "id list of discord ids required (argument 1)");
+invariant(badgeId, "display name of badge is required (argument 2)");
+invariant(
+  discordIds.includes(","),
+  "discordIds must be a comma separated list of discord ids"
+);
+
+const stm = sql.prepare(
+  /* sql */ `insert into "TournamentBadgeOwner" ("badgeId", "userId") values (@badgeId, (select "id" from "User" where "discordId" = @userId))`
+);
+
+const users = discordIds.split(",");
+
+for (const userId of users) {
+  stm.run({ badgeId: Number(badgeId), userId });
+}
+
+console.log(`Added ${users.length} owners to the badge`);
