46449ed08c0b1057b22674608641a346ca3d9dcd
Kalle <38327916+Sendouc@users.noreply.github.com>
2023-03-31 00:16:26 +0300

    Link to user page

--- app/features/placements/queries/findPlacements.server.ts
+++ app/features/placements/queries/findPlacements.server.ts
@@ -1,43 +1,49 @@
 import { sql } from "~/db/sql";
-import type { SplatoonPlacement } from "~/db/types";
+import type { SplatoonPlacement, User } from "~/db/types";
 
 const query = (byPlayer?: boolean) => /* sql */ `
   select
-    "id",
-    "weaponSplId",
-    "name",
-    "power",
-    "rank",
-    "team",
-    "month",
-    "year",
-    "type",
-    "region",
-    "playerId",
-    "month", 
-    "year", 
-    "mode"
+    "SplatoonPlacement"."id",
+    "SplatoonPlacement"."weaponSplId",
+    "SplatoonPlacement"."name",
+    "SplatoonPlacement"."power",
+    "SplatoonPlacement"."rank",
+    "SplatoonPlacement"."team",
+    "SplatoonPlacement"."month",
+    "SplatoonPlacement"."year",
+    "SplatoonPlacement"."type",
+    "SplatoonPlacement"."region",
+    "SplatoonPlacement"."playerId",
+    "SplatoonPlacement"."month", 
+    "SplatoonPlacement"."year", 
+    "SplatoonPlacement"."mode",
+    "User"."discordId",
+    "User"."customUrl"
   from
     "SplatoonPlacement"
+  left join "SplatoonPlayer" on
+    "SplatoonPlayer"."id" = "SplatoonPlacement"."playerId"
+  left join "User" on
+    "User"."id" = "SplatoonPlayer"."userId"
   ${
     byPlayer
       ? /* sql */ `
   where
-    "playerId" = @playerId
+    "SplatoonPlacement"."playerId" = @playerId
   order by
-    "year" desc,
-    "month" desc,
-    "rank" asc
+    "SplatoonPlacement"."year" desc,
+    "SplatoonPlacement"."month" desc,
+    "SplatoonPlacement"."rank" asc
         `
       : /* sql */ `
   where
-    "type" = @type and
-    "mode" = @mode and
-    "region" = @region and
-    "month" = @month and
-    "year" = @year
+    "SplatoonPlacement"."type" = @type and
+    "SplatoonPlacement"."mode" = @mode and
+    "SplatoonPlacement"."region" = @region and
+    "SplatoonPlacement"."month" = @month and
+    "SplatoonPlacement"."year" = @year
   order by
-    "rank" asc`
+    "SplatoonPlacement"."rank" asc`
   }
 `;
 
@@ -60,7 +66,8 @@ export type FindPlacement = Pick<
   | "month"
   | "year"
   | "mode"
->;
+> &
+  Pick<User, "customUrl" | "discordId">;
 
 export function findPlacementsOfMonth(
   args: Pick<SplatoonPlacement, "type" | "mode" | "region" | "month" | "year">
