13e62b56ed91b432f9fcab951bf795027bf75062
Kalle <38327916+Sendouc@users.noreply.github.com>
2023-04-01 22:39:40 +0300

    Add meta

--- app/features/placements/routes/xsearch.player.$id.tsx
+++ app/features/placements/routes/xsearch.player.$id.tsx
@@ -1,4 +1,9 @@
-import type { LinksFunction, LoaderArgs } from "@remix-run/node";
+import type {
+  LinksFunction,
+  LoaderArgs,
+  MetaFunction,
+  SerializeFrom,
+} from "@remix-run/node";
 import { Link, useLoaderData } from "@remix-run/react";
 import { Main } from "~/components/Main";
 import { notFoundIfFalsy } from "~/utils/remix";
@@ -7,17 +12,37 @@ import { findPlacementsByPlayerId } from "../queries/findPlacements.server";
 import styles from "../placements.css";
 import { removeDuplicates } from "~/utils/arrays";
 import { userPage } from "~/utils/urls";
+import { i18next } from "~/modules/i18n";
+import { makeTitle } from "~/utils/strings";
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: styles }];
 };
 
-export const loader = ({ params }: LoaderArgs) => {
+export const meta: MetaFunction = (args) => {
+  const data = args.data as SerializeFrom<typeof loader> | null;
+
+  if (!data) return {};
+
+  return {
+    title: data.title,
+    description: `Splatoon 3 X Battle for the player ${
+      data.placements[0]!.name
+    }`,
+  };
+};
+
+export const loader = async ({ params, request }: LoaderArgs) => {
   const placements = notFoundIfFalsy(
     findPlacementsByPlayerId(Number(params["id"]))
   );
 
-  return { placements };
+  const t = await i18next.getFixedT(request);
+
+  return {
+    placements,
+    title: makeTitle([placements[0]!.name, t("pages.xsearch")]),
+  };
 };
 
 export default function XSearchPlayerPage() {
