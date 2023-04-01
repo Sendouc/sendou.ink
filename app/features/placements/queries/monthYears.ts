f8fd1c9cf0f1841a91b8c4bb1d95675454f02459
Kalle <38327916+Sendouc@users.noreply.github.com>
2023-04-01 23:34:49 +0300

    Fix TODOs related to fetching monthYears

---
+++ app/features/placements/queries/monthYears.ts
@@ -0,0 +1,17 @@
+import { sql } from "~/db/sql";
+import type { MonthYear } from "../placements-utils";
+
+const smt = sql.prepare(/* sql */ `
+  select
+    "month",
+    "year"
+  from "SplatoonPlacement"
+  group by "month", "year"
+  order by "year" desc, "month" desc
+`);
+
+export function monthYears() {
+  const rows = smt.all() as MonthYear[];
+
+  return rows;
+}
