5a28f402c4d4093bfdf1ed48693cf18621ee3e06
Kalle <38327916+Sendouc@users.noreply.github.com>
2023-04-02 15:34:28 +0300

    Add April SR gear

--- scripts/create-gear-json.ts
+++ scripts/create-gear-json.ts
@@ -25,7 +25,7 @@ const LEAN_SHOES_CODE = "Shs";
 // but it's just for clarity
 const AVAILABLE_SR_GEAR = [
   21010, 21011, 21015, 21013, 21012, 21014, 21012, 21000, 21001, 21002, 21001,
-  21002, 21001,
+  21002, 21001, 21016,
 ];
 
 async function main() {
