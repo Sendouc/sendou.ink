ba6607cb5a921f557b18c2cacbdeaff01bd20ce4
zenpk <99558412+zenpk@users.noreply.github.com>
2023-06-14 23:10:05 +0800

    Revert to use solution 1
    This reverts commit 96a6c13e6c30bce5f9197912b56a2f76871276e1.

--- app/components/layout/SideNav.tsx
+++ app/components/layout/SideNav.tsx
@@ -3,47 +3,12 @@ import navItems from "~/components/layout/nav-items.json";
 import { useTranslation } from "~/hooks/useTranslation";
 import { navIconUrl } from "~/utils/urls";
 import { Image } from "../Image";
-import { useEffect, useState } from "react";
 
 export function SideNav() {
   const { t } = useTranslation(["common"]);
-  const [isOverflowed, setIsOverflowed] = useState(false);
-
-  function overflowDetector() {
-    const firstLink = document.querySelector<HTMLAnchorElement>(
-      ".layout__side-nav > :first-child"
-    );
-    const header = document.querySelector<HTMLElement>("header");
-    if (!firstLink || !header) return false;
-    return firstLink.offsetTop < +header.style.height;
-  }
-
-  useEffect(() => {
-    function overflowHandler() {
-      setIsOverflowed(overflowDetector());
-    }
-
-    overflowHandler(); // call it once when the nav is mounted
-
-    window.addEventListener("resize", overflowHandler);
-    return () => {
-      window.removeEventListener("resize", overflowHandler);
-    };
-  }, []); // isOverflowed is not added here to prevent loop
-
-  // every time isOverflowed changes, check it again
-  useEffect(() => {
-    if (overflowDetector()) {
-      setIsOverflowed(true);
-    }
-  }, [isOverflowed]);
 
   return (
-    <nav
-      className={`layout__side-nav layout__item_size ${
-        isOverflowed ? "layout__side-nav-flex-start" : ""
-      }`}
-    >
+    <nav className="layout__side-nav layout__item_size">
       {navItems.map((item) => {
         return (
           <Link
