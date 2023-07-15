5780eaafebdd6be0a9284dc87497cc5c29666648
Kalle <38327916+Sendouc@users.noreply.github.com>
2023-07-15 14:57:45 +0300

    Score reporter initial

--- app/features/sendouq/routes/q.match.$id.tsx
+++ app/features/sendouq/routes/q.match.$id.tsx
@@ -16,6 +16,10 @@ import { useIsMounted } from "~/hooks/useIsMounted";
 import clsx from "clsx";
 import styles from "../q.css";
 import { Avatar } from "~/components/Avatar";
+import { useUser } from "~/modules/auth";
+import * as React from "react";
+import { Flipped, Flipper } from "react-flip-toolkit";
+import { animate } from "~/utils/flip";
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: styles }];
@@ -47,10 +51,23 @@ export const loader = ({ params }: LoaderArgs) => {
 };
 
 // xxx: display team if in the same team, needs migration
+// xxx: weapons? as a row where score report is now
+// xxx: after report show "Alpha won" next to pick info, italic "Unplayed" if not played
+// xxx: handle unranked
 export default function QMatchPage() {
+  const user = useUser();
   const isMounted = useIsMounted();
   const { i18n } = useTranslation();
   const data = useLoaderData<typeof loader>();
+
+  const ownMember =
+    data.groupAlpha.members.find((m) => m.id === user?.id) ??
+    data.groupBravo.members.find((m) => m.id === user?.id);
+  const canReportScore = Boolean(
+    !data.match.reportedAt &&
+      ownMember &&
+      (ownMember.role === "MANAGER" || ownMember.role === "OWNER")
+  );
 
   return (
     <Main className="stack lg">
@@ -80,7 +97,7 @@ export default function QMatchPage() {
         <MatchGroup group={data.groupAlpha} side="ALPHA" />
         <MatchGroup group={data.groupBravo} side="BRAVO" />
       </div>
-      <MapList />
+      <MapList canReportScore={canReportScore} />
     </Main>
   );
 }
@@ -114,9 +131,10 @@ function MatchGroup({
   );
 }
 
-function MapList() {
+function MapList({ canReportScore }: { canReportScore: boolean }) {
   const { t } = useTranslation(["game-misc", "tournament"]);
   const data = useLoaderData<typeof loader>();
+  const [winners, setWinners] = React.useState<("ALPHA" | "BRAVO")[]>([]);
 
   const pickInfo = (source: string) => {
     if (source === "TIEBREAKER") return t("tournament:pickInfo.tiebreaker");
@@ -134,26 +152,100 @@ function MapList() {
     });
   };
 
+  const showWinnerReportRow = (i: number) => {
+    if (!canReportScore) return false;
+
+    if (i === 0) return true;
+
+    const previous = winners[i - 1];
+
+    // xxx: if match is over -> then don't show next... but also handle sometimes them going back and editing scores already reported
+
+    return Boolean(previous);
+  };
+
   return (
-    <div className="stack sm w-max mx-auto">
-      {data.match.mapList.map((map, i) => {
-        return (
-          <div key={i} className="stack sm horizontal items-center">
-            <StageImage
-              stageId={map.stageId}
-              width={64}
-              className="rounded-sm"
-            />
-            <div>
-              <div className="text-sm stack horizontal xs items-center">
-                {i + 1}) <ModeImage mode={map.mode} size={18} />{" "}
-                {t(`game-misc:STAGE_${map.stageId}`)}
-              </div>
-              <div className="text-lighter text-xs">{pickInfo(map.source)}</div>
+    <Flipper flipKey={winners.length}>
+      <div className="stack md w-max mx-auto">
+        {data.match.mapList.map((map, i) => {
+          return (
+            <div key={map.stageId} className="stack xs">
+              <Flipped flipId={map.stageId}>
+                <div className="stack sm horizontal items-center">
+                  <StageImage
+                    stageId={map.stageId}
+                    width={64}
+                    className="rounded-sm"
+                  />
+                  <div>
+                    <div className="text-sm stack horizontal xs items-center">
+                      {i + 1}) <ModeImage mode={map.mode} size={18} />{" "}
+                      {t(`game-misc:STAGE_${map.stageId}`)}
+                    </div>
+                    <div className="text-lighter text-xs">
+                      {pickInfo(map.source)}
+                    </div>
+                  </div>
+                </div>
+              </Flipped>
+              {showWinnerReportRow(i) ? (
+                <Flipped
+                  flipId={`${map.stageId}-report`}
+                  // eslint-disable-next-line @typescript-eslint/no-misused-promises
+                  onAppear={async (el: HTMLElement) => {
+                    await animate(el, [{ opacity: 0 }, { opacity: 1 }], {
+                      duration: 300,
+                    });
+                    el.style.opacity = "1";
+                  }}
+                >
+                  <div className="stack horizontal sm text-xs">
+                    <label className="mb-0 text-theme-secondary">Winner</label>
+                    <div className="stack sm horizontal items-center font-semi-bold">
+                      <input
+                        type="radio"
+                        name={`winner-${i}`}
+                        value="alpha"
+                        id={`alpha-${i}`}
+                        checked={winners[i] === "ALPHA"}
+                        onChange={() => {
+                          setWinners((prev) => {
+                            const newWinners = [...prev];
+                            newWinners[i] = "ALPHA";
+                            return newWinners;
+                          });
+                        }}
+                      />
+                      <label className="mb-0" htmlFor={`alpha-${i}`}>
+                        Alpha
+                      </label>
+                    </div>
+                    <div className="stack sm horizontal items-center font-semi-bold">
+                      <input
+                        type="radio"
+                        name={`winner-${i}`}
+                        value="bravo"
+                        id={`bravo-${i}`}
+                        checked={winners[i] === "BRAVO"}
+                        onChange={() => {
+                          setWinners((prev) => {
+                            const newWinners = [...prev];
+                            newWinners[i] = "BRAVO";
+                            return newWinners;
+                          });
+                        }}
+                      />
+                      <label className="mb-0" htmlFor={`bravo-${i}`}>
+                        Bravo
+                      </label>
+                    </div>
+                  </div>
+                </Flipped>
+              ) : null}
             </div>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+      </div>
+    </Flipper>
   );
 }
