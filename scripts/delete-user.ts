ad54edf05e591978e17b59144411f806761f3b94
Kalle <38327916+Sendouc@users.noreply.github.com>
2023-07-28 22:43:15 +0300

    Script to delete user Closes #1430

---
+++ scripts/delete-user.ts
@@ -0,0 +1,14 @@
+/* eslint-disable no-console */
+import "dotenv/config";
+import invariant from "tiny-invariant";
+import { sql } from "~/db/sql";
+
+const discordId = process.argv[2]?.trim();
+
+invariant(discordId, "discord id is required (argument 1)");
+
+sql
+  .prepare('delete from "User" where discordId = @discordId')
+  .run({ discordId });
+
+console.log(`Deleted user with discord id: ${discordId}`);
