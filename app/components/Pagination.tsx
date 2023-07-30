6d3093c9fd286af95272bf3a05da5de82d13cc98
Kalle <38327916+Sendouc@users.noreply.github.com>
2023-07-30 11:03:11 +0300

    Extract Pagination component

---
+++ app/components/Pagination.tsx
@@ -0,0 +1,47 @@
+import clsx from "clsx";
+import { Button } from "~/components/Button";
+import { ArrowLeftIcon } from "~/components/icons/ArrowLeft";
+import { ArrowRightIcon } from "~/components/icons/ArrowRight";
+import { nullFilledArray } from "~/utils/arrays";
+
+export function Pagination({
+  currentPage,
+  pagesCount,
+  nextPage,
+  previousPage,
+  setPage,
+}: {
+  currentPage: number;
+  pagesCount: number;
+  nextPage: () => void;
+  previousPage: () => void;
+  setPage: (page: number) => void;
+}) {
+  return (
+    <div className="stack sm horizontal items-center justify-center flex-wrap">
+      <Button
+        icon={<ArrowLeftIcon />}
+        variant="outlined"
+        disabled={currentPage === 1}
+        onClick={previousPage}
+        aria-label="Previous page"
+      />
+      {nullFilledArray(pagesCount).map((_, i) => (
+        <div
+          key={i}
+          className={clsx("pagination__dot", {
+            pagination__dot__active: i === currentPage - 1,
+          })}
+          onClick={() => setPage(i + 1)}
+        />
+      ))}
+      <Button
+        icon={<ArrowRightIcon />}
+        variant="outlined"
+        disabled={currentPage === pagesCount}
+        onClick={nextPage}
+        aria-label="Next page"
+      />
+    </div>
+  );
+}
