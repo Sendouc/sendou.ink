3412597fd939da39a581ee200f5690a4c520b26c
Kalle <38327916+Sendouc@users.noreply.github.com>
2023-09-26 23:59:51 +0300

    Change session cookie name

--- app/modules/auth/session.server.ts
+++ app/modules/auth/session.server.ts
@@ -8,8 +8,7 @@ if (process.env.NODE_ENV === "production") {
 }
 export const authSessionStorage = createCookieSessionStorage({
   cookie: {
-    // xxx: if domain trick works this needs renaming to force people to log back in
-    name: "_session",
+    name: "__session",
     sameSite: "lax",
     // need to specify domain so that sub-domains can access it
     domain: process.env.NODE_ENV === "production" ? "sendou.ink" : undefined,
