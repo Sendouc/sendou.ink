776291d464f902e1b2f27962fc7db29d641858d7
Kalle <38327916+Sendouc@users.noreply.github.com>
2023-10-22 12:34:53 +0300

    Kysely initial

--- app/db/sql.ts
+++ app/db/sql.ts
@@ -1,5 +1,7 @@
 import Database from "better-sqlite3";
 import invariant from "tiny-invariant";
+import { Kysely, SqliteDialect } from "kysely";
+import type { DB } from "kysely-codegen";
 
 invariant(process.env["DB_PATH"], "DB_PATH env variable must be set");
 export const sql = new Database(process.env["DB_PATH"]);
@@ -7,3 +9,10 @@ export const sql = new Database(process.env["DB_PATH"]);
 sql.pragma("journal_mode = WAL");
 sql.pragma("foreign_keys = ON");
 sql.pragma("busy_timeout = 5000");
+
+// xxx: rename
+export const dbNew = new Kysely<DB>({
+  dialect: new SqliteDialect({
+    database: sql,
+  }),
+});
