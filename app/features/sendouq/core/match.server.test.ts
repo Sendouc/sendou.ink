2f4b50176ec1de855ceb7ab2620cb50af027794b
Kalle <38327916+Sendouc@users.noreply.github.com>
2023-11-25 12:02:15 +0200

    Unskip tests

--- app/features/sendouq/core/match.server.test.ts
+++ app/features/sendouq/core/match.server.test.ts
@@ -5,7 +5,8 @@ import {
   mapPoolFromPreferences,
 } from "./match.server";
 import * as Test from "~/utils/Test";
-import { stageIds } from "~/modules/in-game-lists";
+import { type ModeShort, stageIds } from "~/modules/in-game-lists";
+import { BANNED_MAPS } from "~/features/sendouq-settings/banned-maps";
 
 const MapModePreferencesToModeList = suite("mapModePreferencesToModeList()");
 const MapPoolFromPreferences = suite("mapPoolFromPreferences()");
@@ -144,16 +145,31 @@ MapPoolFromPreferences("returns maps even if no preferences", () => {
   assert.equal(mapPool.stageModePairs.length, STAGES_PER_MODE * MODES_COUNT);
 });
 
-// xxx: take in account map bans
-const MAX_STAGE_ID = Math.max(...stageIds);
-MapPoolFromPreferences.skip(
+MapPoolFromPreferences(
   "tiebreaker if tied preference is stage id (bigger preferred)",
   () => {
+    const minIdConsiderBans = (mode: ModeShort) => {
+      const MAX_STAGE_ID = Math.max(...stageIds);
+
+      let id = MAX_STAGE_ID;
+      let stagesToPick = STAGES_PER_MODE;
+      for (const stageId of [...stageIds].reverse()) {
+        if (stagesToPick === 0) break;
+        id--;
+
+        if (BANNED_MAPS[mode].includes(stageId)) continue;
+
+        stagesToPick--;
+      }
+
+      return id;
+    };
+
     const mapPool = mapPoolFromPreferences([]);
 
     assert.ok(
       mapPool.stageModePairs.every(
-        ({ stageId }) => stageId > MAX_STAGE_ID - STAGES_PER_MODE,
+        ({ stageId, mode }) => stageId >= minIdConsiderBans(mode),
       ),
     );
   },
@@ -218,29 +234,55 @@ MapPoolFromPreferences(
   },
 );
 
-// xxx: make dynamic with map bans
-MapPoolFromPreferences.skip("works across multiple modes", () => {
+MapPoolFromPreferences("works across multiple modes", () => {
+  const findFirstLegalMapFromMode = (mode: ModeShort) => {
+    for (const stageId of stageIds) {
+      if (BANNED_MAPS[mode].includes(stageId)) continue;
+
+      return stageId;
+    }
+
+    throw new Error("No legal map found");
+  };
+
   const mapPool = mapPoolFromPreferences([
     [
-      { stageId: 6, preference: "PREFER", mode: "SZ" },
-      { stageId: 7, preference: "PREFER", mode: "TC" },
+      {
+        stageId: findFirstLegalMapFromMode("SZ"),
+        preference: "PREFER",
+        mode: "SZ",
+      },
+      {
+        stageId: findFirstLegalMapFromMode("TC"),
+        preference: "PREFER",
+        mode: "TC",
+      },
     ],
-    [{ stageId: 8, preference: "PREFER", mode: "RM" }],
+    [
+      {
+        stageId: findFirstLegalMapFromMode("RM"),
+        preference: "PREFER",
+        mode: "RM",
+      },
+    ],
   ]);
 
   assert.ok(
     mapPool.stageModePairs.some(
-      (pair) => pair.stageId === 6 && pair.mode === "SZ",
+      (pair) =>
+        pair.stageId === findFirstLegalMapFromMode("SZ") && pair.mode === "SZ",
     ),
   );
   assert.ok(
     mapPool.stageModePairs.some(
-      (pair) => pair.stageId === 7 && pair.mode === "TC",
+      (pair) =>
+        pair.stageId === findFirstLegalMapFromMode("TC") && pair.mode === "TC",
     ),
   );
   assert.ok(
     mapPool.stageModePairs.some(
-      (pair) => pair.stageId === 8 && pair.mode === "RM",
+      (pair) =>
+        pair.stageId === findFirstLegalMapFromMode("RM") && pair.mode === "RM",
     ),
   );
 });
