7a613bd1daacd662d1c02acc95e843a3ad0de5a0
Kalle <38327916+Sendouc@users.noreply.github.com>
2023-11-25 14:04:26 +0200

    tsconfig ES2020

---
+++ app/features/calendar/calendar-utils.ts
@@ -0,0 +1,4 @@
+import { userDiscordIdIsAged } from "~/utils/users";
+
+export const canAddNewEvent = (user: { discordId: string }) =>
+  userDiscordIdIsAged(user);
--- app/utils/users.ts
+++ app/utils/users.ts
@@ -38,3 +38,30 @@ export function queryToUserIdentifier(
 
   return null;
 }
+
+// snowflake logic from https://github.dev/vegeta897/snow-stamp/blob/main/src/util.js
+const DISCORD_EPOCH = 1420070400000;
+
+// Converts a snowflake ID string into a JS Date object using the provided epoch (in ms), or Discord's epoch if not provided
+function convertSnowflakeToDate(snowflake: string) {
+  // Convert snowflake to BigInt to extract timestamp bits
+  // https://discord.com/developers/docs/reference#snowflakes
+  const milliseconds = BigInt(snowflake) >> 22n;
+  return new Date(Number(milliseconds) + DISCORD_EPOCH);
+}
+
+const AGED_CRITERIA = 1000 * 60 * 60 * 24 * 30 * 3; // 3 months
+export function userDiscordIdIsAged(user: { discordId: string }) {
+  // types should catch this but since this is a permission related
+  // code playing it safe
+  if (!user.discordId) {
+    throw new Error("No discord id");
+  }
+  if (user.discordId.length < DISCORD_ID_MIN_LENGTH) {
+    throw new Error("Not a valid discord id");
+  }
+
+  const timestamp = convertSnowflakeToDate(user.discordId).getTime();
+
+  return Date.now() - timestamp > AGED_CRITERIA;
+}
