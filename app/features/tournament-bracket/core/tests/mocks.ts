e01cf5660428c4379a09896395c1d58905cf0eb1
Kalle <38327916+Sendouc@users.noreply.github.com>
2024-01-22 21:33:49 +0200

    Add RR tests

--- app/features/tournament-bracket/core/tests/mocks.ts
+++ app/features/tournament-bracket/core/tests/mocks.ts
@@ -13,7 +13,7 @@ export const FOUR_TEAMS_RR = (): ValueToArray<DataTypes> => ({
         roundRobinMode: "simple",
         matchesChildCount: 0,
         size: 4,
-        seedOrdering: ["groups.effort_balanced"],
+        seedOrdering: ["groups.seed_optimized"],
       },
     },
   ],
@@ -169,6 +169,264 @@ export const FOUR_TEAMS_RR = (): ValueToArray<DataTypes> => ({
       id: 3,
       tournament_id: 1,
       name: "Team 4",
+    },
+  ],
+});
+
+export const FIVE_TEAMS_RR = (): ValueToArray<DataTypes> => ({
+  stage: [
+    {
+      id: 0,
+      tournament_id: 3,
+      name: "Groups stage",
+      type: "round_robin",
+      number: 1,
+      settings: {
+        groupCount: 1,
+        seedOrdering: ["groups.seed_optimized"],
+        roundRobinMode: "simple",
+        matchesChildCount: 0,
+        size: 5,
+      },
+    },
+  ],
+  group: [
+    {
+      id: 0,
+      stage_id: 0,
+      number: 1,
+    },
+  ],
+  round: [
+    {
+      id: 0,
+      number: 1,
+      stage_id: 0,
+      group_id: 0,
+    },
+    {
+      id: 1,
+      number: 2,
+      stage_id: 0,
+      group_id: 0,
+    },
+    {
+      id: 2,
+      number: 3,
+      stage_id: 0,
+      group_id: 0,
+    },
+    {
+      id: 3,
+      number: 4,
+      stage_id: 0,
+      group_id: 0,
+    },
+    {
+      id: 4,
+      number: 5,
+      stage_id: 0,
+      group_id: 0,
+    },
+  ],
+  match: [
+    {
+      id: 0,
+      number: 1,
+      stage_id: 0,
+      group_id: 0,
+      round_id: 0,
+      child_count: 0,
+      status: 2,
+      opponent1: {
+        id: 4,
+        position: 5,
+      },
+      opponent2: {
+        id: 1,
+        position: 2,
+      },
+    },
+    {
+      id: 1,
+      number: 2,
+      stage_id: 0,
+      group_id: 0,
+      round_id: 0,
+      child_count: 0,
+      status: 2,
+      opponent1: {
+        id: 3,
+        position: 4,
+      },
+      opponent2: {
+        id: 2,
+        position: 3,
+      },
+    },
+    {
+      id: 2,
+      number: 1,
+      stage_id: 0,
+      group_id: 0,
+      round_id: 1,
+      child_count: 0,
+      status: 2,
+      opponent1: {
+        id: 0,
+        position: 1,
+      },
+      opponent2: {
+        id: 2,
+        position: 3,
+      },
+    },
+    {
+      id: 3,
+      number: 2,
+      stage_id: 0,
+      group_id: 0,
+      round_id: 1,
+      child_count: 0,
+      status: 2,
+      opponent1: {
+        id: 4,
+        position: 5,
+      },
+      opponent2: {
+        id: 3,
+        position: 4,
+      },
+    },
+    {
+      id: 4,
+      number: 1,
+      stage_id: 0,
+      group_id: 0,
+      round_id: 2,
+      child_count: 0,
+      status: 2,
+      opponent1: {
+        id: 1,
+        position: 2,
+      },
+      opponent2: {
+        id: 3,
+        position: 4,
+      },
+    },
+    {
+      id: 5,
+      number: 2,
+      stage_id: 0,
+      group_id: 0,
+      round_id: 2,
+      child_count: 0,
+      status: 2,
+      opponent1: {
+        id: 0,
+        position: 1,
+      },
+      opponent2: {
+        id: 4,
+        position: 5,
+      },
+    },
+    {
+      id: 6,
+      number: 1,
+      stage_id: 0,
+      group_id: 0,
+      round_id: 3,
+      child_count: 0,
+      status: 2,
+      opponent1: {
+        id: 2,
+        position: 3,
+      },
+      opponent2: {
+        id: 4,
+        position: 5,
+      },
+    },
+    {
+      id: 7,
+      number: 2,
+      stage_id: 0,
+      group_id: 0,
+      round_id: 3,
+      child_count: 0,
+      status: 2,
+      opponent1: {
+        id: 1,
+        position: 2,
+      },
+      opponent2: {
+        id: 0,
+        position: 1,
+      },
+    },
+    {
+      id: 8,
+      number: 1,
+      stage_id: 0,
+      group_id: 0,
+      round_id: 4,
+      child_count: 0,
+      status: 2,
+      opponent1: {
+        id: 3,
+        position: 4,
+      },
+      opponent2: {
+        id: 0,
+        position: 1,
+      },
+    },
+    {
+      id: 9,
+      number: 2,
+      stage_id: 0,
+      group_id: 0,
+      round_id: 4,
+      child_count: 0,
+      status: 2,
+      opponent1: {
+        id: 2,
+        position: 3,
+      },
+      opponent2: {
+        id: 1,
+        position: 2,
+      },
+    },
+  ],
+  match_game: [],
+  participant: [
+    {
+      id: 0,
+      tournament_id: 3,
+      name: "Team 1",
+    },
+    {
+      id: 1,
+      tournament_id: 3,
+      name: "Team 2",
+    },
+    {
+      id: 2,
+      tournament_id: 3,
+      name: "Team 3",
+    },
+    {
+      id: 3,
+      tournament_id: 3,
+      name: "Team 4",
+    },
+    {
+      id: 4,
+      tournament_id: 3,
+      name: "Team 5",
     },
   ],
 });
--- app/features/tournament-bracket/core/tests/round-robin.test.ts
+++ app/features/tournament-bracket/core/tests/round-robin.test.ts
@@ -1,5 +1,5 @@
 import { suite } from "uvu";
-import { FOUR_TEAMS_RR } from "./mocks";
+import { FIVE_TEAMS_RR, FOUR_TEAMS_RR } from "./mocks";
 import { adjustResults, testTournament } from "./test-utils";
 import * as assert from "uvu/assert";
 
@@ -27,7 +27,87 @@ RoundRobinStandings.skip("resolves standings from points", () => {
   assert.equal(standings[3].team.id, 3);
 });
 
-RoundRobinStandings.skip("tiebreaker via head-to-head", () => {});
+RoundRobinStandings.skip("tiebreaker via head-to-head", () => {
+  // id 0 = WWWW
+  // id 1 = WWWL
+  // id 2 = WWLL
+  // id 3 = WWLL but won against 2
+  // id 4 = LLLL
+  const tournament = testTournament(
+    adjustResults(FIVE_TEAMS_RR(), [
+      {
+        ids: [4, 1],
+        score: [0, 2],
+      },
+      {
+        ids: [3, 2],
+        score: [2, 0],
+      },
+      {
+        ids: [0, 2],
+        score: [2, 0],
+      },
+      {
+        ids: [4, 3],
+        score: [0, 2],
+      },
+      {
+        ids: [1, 3],
+        score: [2, 0],
+      },
+      {
+        ids: [0, 4],
+        score: [2, 0],
+      },
+      {
+        ids: [2, 4],
+        score: [2, 0],
+      },
+      {
+        ids: [1, 0],
+        score: [0, 2],
+      },
+      {
+        ids: [3, 0],
+        score: [0, 2],
+      },
+      {
+        ids: [2, 1],
+        score: [2, 0],
+      },
+    ]),
+  );
+
+  const standings = tournament.standings;
+
+  assert.equal(standings.length, 5);
+  assert.equal(standings[2].team.id, 3);
+  assert.equal(standings[2].placement, 3);
+  assert.equal(standings[3].team.id, 2);
+  assert.equal(standings[3].placement, 4);
+});
+
+RoundRobinStandings.skip("tiebreaker via maps won", () => {
+  // id 0 = WWWW
+  // id 1 = WWLL
+  // id 2 = WWLL
+  // id 3 = WWLL
+  const tournament = testTournament(
+    adjustResults(FOUR_TEAMS_RR(), [
+      { ids: [0, 3], score: [2, 0] },
+      { ids: [2, 1], score: [0, 2] },
+      { ids: [1, 3], score: [0, 2] },
+      { ids: [0, 2], score: [2, 0] },
+      { ids: [2, 3], score: [2, 1] },
+      { ids: [1, 0], score: [0, 2] },
+    ]),
+  );
+
+  const standings = tournament.standings;
+
+  // they won the most maps out of the 3 tied teams
+  assert.equal(standings[1].team.id, 3);
+});
 
 RoundRobinStandings.skip("three way tiebreaker via points scored", () => {});
 
