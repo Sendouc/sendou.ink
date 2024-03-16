c1a8df1ba685849a4d63c14f0a89b389e3c6b54a
Kalle <38327916+Sendouc@users.noreply.github.com>
2024-03-16 19:03:30 +0200

    Unlink player script

---
+++ scripts/unlink-player.ts
@@ -0,0 +1,18 @@
+/* eslint-disable no-console */
+import "dotenv/config";
+import invariant from "tiny-invariant";
+import { sql } from "~/db/sql";
+import { syncXPBadges } from "../app/features/badges/queries/syncXPBadges.server";
+
+const discordId = process.argv[2]?.trim();
+
+invariant(discordId, "discord id is required (argument 1)");
+
+sql
+  .prepare(
+    'update "SplatoonPlayer" set userId = null where userId = (select id from "User" where discordId = @discordId)',
+  )
+  .run({ discordId });
+syncXPBadges();
+
+console.log(`Unlinked player for discord id: ${discordId}`);
