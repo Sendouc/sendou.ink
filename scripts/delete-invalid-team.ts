6d68eecaae292fc34d1028295aa253a2dea82f6e
Kalle <38327916+Sendouc@users.noreply.github.com>
2024-04-07 15:59:39 +0300

    Remove unneeded script

--- scripts/delete-invalid-team.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-console */
-import "dotenv/config";
-import { sql } from "~/db/sql";
-
-sql.prepare(`delete from "AllTeam" where "customUrl" = ''`).run();
-
-console.log(`Done deleting empty team`);
