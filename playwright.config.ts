3e5e0ff70c655dd26de7c91c29bd80c4531bb109
Kalle <38327916+Sendouc@users.noreply.github.com>
2024-04-16 22:26:43 +0300

    Try built

--- playwright.config.ts
+++ playwright.config.ts
@@ -100,7 +100,7 @@ const config: PlaywrightTestConfig = {
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "npm run dev:ci",
+    command: process.env["CI"] ? "npm start" : "npm run dev:ci",
     port: 5173,
     reuseExistingServer: !process.env["CI"],
   },
