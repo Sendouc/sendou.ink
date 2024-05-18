335fe487d26ad18e18dcd4fa6939211b13a0ce1f
Kalle <38327916+Sendouc@users.noreply.github.com>
2024-05-18 10:26:53 +0300

    Script to disband team

---
+++ scripts/disband-team.ts
@@ -0,0 +1,24 @@
+import "dotenv/config";
+import invariant from "tiny-invariant";
+import { db } from "~/db/sql";
+import { dateToDatabaseTimestamp } from "~/utils/dates";
+import { logger } from "~/utils/logger";
+
+const id = Number(process.argv[2]?.trim());
+
+invariant(id, "team id is required (argument 1)");
+invariant(Number.isInteger(id), "team id must be an integer");
+
+async function main() {
+  await db
+    .updateTable("AllTeam")
+    .set({
+      deletedAt: dateToDatabaseTimestamp(new Date()),
+    })
+    .where("id", "=", id)
+    .execute();
+
+  logger.info(`Disbanded team with id: ${id}`);
+}
+
+void main();
