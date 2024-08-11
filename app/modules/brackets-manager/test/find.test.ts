6e467438b073f698628745ce133319145ea65a8a
Kalle <38327916+Sendouc@users.noreply.github.com>
2024-08-11 11:33:51 +0300

    test

--- app/modules/brackets-manager/test/find.test.ts
+++ app/modules/brackets-manager/test/find.test.ts
@@ -1,69 +1,63 @@
-import { suite } from "uvu";
-import * as assert from "uvu/assert";
+import { describe, test, expect, beforeEach } from "bun:test";
 import { InMemoryDatabase } from "~/modules/brackets-memory-db";
 import { BracketsManager } from "../manager";
 
 const storage = new InMemoryDatabase();
 const manager = new BracketsManager(storage);
 
-const FindSingleElimination = suite(
-	"Find previous and next matches in single elimination",
-);
-
-FindSingleElimination.before.each(() => {
-	storage.reset();
-});
-
-FindSingleElimination("should find previous matches", () => {
-	manager.create({
-		name: "Example",
-		tournamentId: 0,
-		type: "single_elimination",
-		seeding: [1, 2, 3, 4, 5, 6, 7, 8],
+describe("Find previous and next matches in single elimination", () => {
+	beforeEach(() => {
+		storage.reset();
 	});
 
-	const beforeFirst = manager.find.previousMatches(0);
-	assert.equal(beforeFirst.length, 0);
+	test("should find previous matches", () => {
+		manager.create({
+			name: "Example",
+			tournamentId: 0,
+			type: "single_elimination",
+			seeding: [1, 2, 3, 4, 5, 6, 7, 8],
+		});
 
-	const beforeSemi1 = manager.find.previousMatches(4);
-	assert.equal(beforeSemi1.length, 2);
-	assert.equal(beforeSemi1[0].id, 0);
-	assert.equal(beforeSemi1[1].id, 1);
+		const beforeFirst = manager.find.previousMatches(0);
+		expect(beforeFirst.length).toBe(0);
 
-	const beforeSemi2 = manager.find.previousMatches(5);
-	assert.equal(beforeSemi2.length, 2);
-	assert.equal(beforeSemi2[0].id, 2);
-	assert.equal(beforeSemi2[1].id, 3);
+		const beforeSemi1 = manager.find.previousMatches(4);
+		expect(beforeSemi1.length).toBe(2);
+		expect(beforeSemi1[0].id).toBe(0);
+		expect(beforeSemi1[1].id).toBe(1);
 
-	const beforeFinal = manager.find.previousMatches(6);
-	assert.equal(beforeFinal.length, 2);
-	assert.equal(beforeFinal[0].id, 4);
-	assert.equal(beforeFinal[1].id, 5);
-});
+		const beforeSemi2 = manager.find.previousMatches(5);
+		expect(beforeSemi2.length).toBe(2);
+		expect(beforeSemi2[0].id).toBe(2);
+		expect(beforeSemi2[1].id).toBe(3);
 
-FindSingleElimination("should find next matches", () => {
-	manager.create({
-		name: "Example",
-		tournamentId: 0,
-		type: "single_elimination",
-		seeding: [1, 2, 3, 4, 5, 6, 7, 8],
+		const beforeFinal = manager.find.previousMatches(6);
+		expect(beforeFinal.length).toBe(2);
+		expect(beforeFinal[0].id).toBe(4);
+		expect(beforeFinal[1].id).toBe(5);
 	});
 
-	const afterFirst = manager.find.nextMatches(0);
-	assert.equal(afterFirst.length, 1);
-	assert.equal(afterFirst[0].id, 4);
+	test("should find next matches", () => {
+		manager.create({
+			name: "Example",
+			tournamentId: 0,
+			type: "single_elimination",
+			seeding: [1, 2, 3, 4, 5, 6, 7, 8],
+		});
 
-	const afterSemi1 = manager.find.nextMatches(4);
-	assert.equal(afterSemi1.length, 1);
-	assert.equal(afterSemi1[0].id, 6);
+		const afterFirst = manager.find.nextMatches(0);
+		expect(afterFirst.length).toBe(1);
+		expect(afterFirst[0].id).toBe(4);
 
-	const afterFinal = manager.find.nextMatches(6);
-	assert.equal(afterFinal.length, 0);
-});
+		const afterSemi1 = manager.find.nextMatches(4);
+		expect(afterSemi1.length).toBe(1);
+		expect(afterSemi1[0].id).toBe(6);
 
-FindSingleElimination(
-	"should return matches from the point of view of a participant",
-	() => {
+		const afterFinal = manager.find.nextMatches(6);
+		expect(afterFinal.length).toBe(0);
+	});
+
+	test("should return matches from the point of view of a participant", () => {
 		manager.create({
 			name: "Example",
 			tournamentId: 0,
@@ -76,117 +70,112 @@ FindSingleElimination(
 
 		manager.update.match({ id: 0, opponent1: { result: "loss" } });
 		const afterFirstEliminated = manager.find.nextMatches(0, 1);
-		assert.equal(afterFirstEliminated.length, 0);
+		expect(afterFirstEliminated.length).toBe(0);
 		const afterFirstContinued = manager.find.nextMatches(0, 2);
-		assert.equal(afterFirstContinued.length, 1);
+		expect(afterFirstContinued.length).toBe(1);
 
 		manager.update.match({ id: 1, opponent1: { result: "win" } });
 		const beforeSemi1Up = manager.find.previousMatches(4, 2);
-		assert.equal(beforeSemi1Up.length, 1);
-		assert.equal(beforeSemi1Up[0].id, 0);
+		expect(beforeSemi1Up.length).toBe(1);
+		expect(beforeSemi1Up[0].id).toBe(0);
 
 		const beforeSemi1Down = manager.find.previousMatches(4, 3);
-		assert.equal(beforeSemi1Down.length, 1);
-		assert.equal(beforeSemi1Down[0].id, 1);
-	},
-);
-
-const FindDoubleElimination = suite(
-	"Find previous and next matches in double elimination",
-);
-
-FindDoubleElimination.before.each(() => {
-	storage.reset();
+		expect(beforeSemi1Down.length).toBe(1);
+		expect(beforeSemi1Down[0].id).toBe(1);
+	});
 });
 
-FindDoubleElimination("should find previous matches", () => {
-	manager.create({
-		name: "Example",
-		tournamentId: 0,
-		type: "double_elimination",
-		seeding: [1, 2, 3, 4, 5, 6, 7, 8],
+describe("Find previous and next matches in double elimination", () => {
+	beforeEach(() => {
+		storage.reset();
 	});
 
-	const beforeFirstWB = manager.find.previousMatches(0);
-	assert.equal(beforeFirstWB.length, 0);
+	test("should find previous matches", () => {
+		manager.create({
+			name: "Example",
+			tournamentId: 0,
+			type: "double_elimination",
+			seeding: [1, 2, 3, 4, 5, 6, 7, 8],
+		});
 
-	const beforeSemi1WB = manager.find.previousMatches(4);
-	assert.equal(beforeSemi1WB.length, 2);
-	assert.equal(beforeSemi1WB[0].id, 0);
-	assert.equal(beforeSemi1WB[1].id, 1);
+		const beforeFirstWB = manager.find.previousMatches(0);
+		expect(beforeFirstWB.length).toBe(0);
 
-	const beforeSemi2WB = manager.find.previousMatches(5);
-	assert.equal(beforeSemi2WB.length, 2);
-	assert.equal(beforeSemi2WB[0].id, 2);
-	assert.equal(beforeSemi2WB[1].id, 3);
+		const beforeSemi1WB = manager.find.previousMatches(4);
+		expect(beforeSemi1WB.length).toBe(2);
+		expect(beforeSemi1WB[0].id).toBe(0);
+		expect(beforeSemi1WB[1].id).toBe(1);
 
-	const beforeFinalWB = manager.find.previousMatches(6);
-	assert.equal(beforeFinalWB.length, 2);
-	assert.equal(beforeFinalWB[0].id, 4);
-	assert.equal(beforeFinalWB[1].id, 5);
+		const beforeSemi2WB = manager.find.previousMatches(5);
+		expect(beforeSemi2WB.length).toBe(2);
+		expect(beforeSemi2WB[0].id).toBe(2);
+		expect(beforeSemi2WB[1].id).toBe(3);
 
-	const beforeFirstRound1LB = manager.find.previousMatches(7);
-	assert.equal(beforeFirstRound1LB.length, 2);
-	assert.equal(beforeFirstRound1LB[0].id, 0);
-	assert.equal(beforeFirstRound1LB[1].id, 1);
+		const beforeFinalWB = manager.find.previousMatches(6);
+		expect(beforeFinalWB.length).toBe(2);
+		expect(beforeFinalWB[0].id).toBe(4);
+		expect(beforeFinalWB[1].id).toBe(5);
 
-	const beforeFirstRound2LB = manager.find.previousMatches(9);
-	assert.equal(beforeFirstRound2LB.length, 2);
-	assert.equal(beforeFirstRound2LB[0].id, 5);
-	assert.equal(beforeFirstRound2LB[1].id, 7);
+		const beforeFirstRound1LB = manager.find.previousMatches(7);
+		expect(beforeFirstRound1LB.length).toBe(2);
+		expect(beforeFirstRound1LB[0].id).toBe(0);
+		expect(beforeFirstRound1LB[1].id).toBe(1);
 
-	const beforeSemi1LB = manager.find.previousMatches(11);
-	assert.equal(beforeSemi1LB.length, 2);
-	assert.equal(beforeSemi1LB[0].id, 9);
-	assert.equal(beforeSemi1LB[1].id, 10);
+		const beforeFirstRound2LB = manager.find.previousMatches(9);
+		expect(beforeFirstRound2LB.length).toBe(2);
+		expect(beforeFirstRound2LB[0].id).toBe(5);
+		expect(beforeFirstRound2LB[1].id).toBe(7);
 
-	const beforeFinalLB = manager.find.previousMatches(12);
-	assert.equal(beforeFinalLB.length, 2);
-	assert.equal(beforeFinalLB[0].id, 6);
-	assert.equal(beforeFinalLB[1].id, 11);
-});
+		const beforeSemi1LB = manager.find.previousMatches(11);
+		expect(beforeSemi1LB.length).toBe(2);
+		expect(beforeSemi1LB[0].id).toBe(9);
+		expect(beforeSemi1LB[1].id).toBe(10);
 
-FindDoubleElimination("should find next matches", () => {
-	manager.create({
-		name: "Example",
-		tournamentId: 0,
-		type: "double_elimination",
-		seeding: [1, 2, 3, 4, 5, 6, 7, 8],
+		const beforeFinalLB = manager.find.previousMatches(12);
+		expect(beforeFinalLB.length).toBe(2);
+		expect(beforeFinalLB[0].id).toBe(6);
+		expect(beforeFinalLB[1].id).toBe(11);
 	});
 
-	const afterFirstWB = manager.find.nextMatches(0);
-	assert.equal(afterFirstWB.length, 2);
-	assert.equal(afterFirstWB[0].id, 4);
-	assert.equal(afterFirstWB[1].id, 7);
+	test("should find next matches", () => {
+		manager.create({
+			name: "Example",
+			tournamentId: 0,
+			type: "double_elimination",
+			seeding: [1, 2, 3, 4, 5, 6, 7, 8],
+		});
 
-	const afterSemi1WB = manager.find.nextMatches(4);
-	assert.equal(afterSemi1WB.length, 2);
-	assert.equal(afterSemi1WB[0].id, 6);
-	assert.equal(afterSemi1WB[1].id, 10);
+		const afterFirstWB = manager.find.nextMatches(0);
+		expect(afterFirstWB.length).toBe(2);
+		expect(afterFirstWB[0].id).toBe(4);
+		expect(afterFirstWB[1].id).toBe(7);
 
-	const afterFinalWB = manager.find.nextMatches(6);
-	assert.equal(afterFinalWB.length, 1);
-	assert.equal(afterFinalWB[0].id, 12);
+		const afterSemi1WB = manager.find.nextMatches(4);
+		expect(afterSemi1WB.length).toBe(2);
+		expect(afterSemi1WB[0].id).toBe(6);
+		expect(afterSemi1WB[1].id).toBe(10);
 
-	const afterFirstRound1LB = manager.find.nextMatches(7);
-	assert.equal(afterFirstRound1LB.length, 1);
-	assert.equal(afterFirstRound1LB[0].id, 9);
+		const afterFinalWB = manager.find.nextMatches(6);
+		expect(afterFinalWB.length).toBe(1);
+		expect(afterFinalWB[0].id).toBe(12);
 
-	const afterFirstRound2LB = manager.find.nextMatches(9);
-	assert.equal(afterFirstRound2LB.length, 1);
-	assert.equal(afterFirstRound2LB[0].id, 11);
+		const afterFirstRound1LB = manager.find.nextMatches(7);
+		expect(afterFirstRound1LB.length).toBe(1);
+		expect(afterFirstRound1LB[0].id).toBe(9);
 
-	const afterSemi1LB = manager.find.nextMatches(11);
-	assert.equal(afterSemi1LB.length, 1);
-	assert.equal(afterSemi1LB[0].id, 12);
+		const afterFirstRound2LB = manager.find.nextMatches(9);
+		expect(afterFirstRound2LB.length).toBe(1);
+		expect(afterFirstRound2LB[0].id).toBe(11);
 
-	const afterFinalLB = manager.find.nextMatches(12);
-	assert.equal(afterFinalLB.length, 0);
-});
+		const afterSemi1LB = manager.find.nextMatches(11);
+		expect(afterSemi1LB.length).toBe(1);
+		expect(afterSemi1LB[0].id).toBe(12);
 
-FindDoubleElimination(
-	"should return matches from the point of view of a participant",
-	() => {
+		const afterFinalLB = manager.find.nextMatches(12);
+		expect(afterFinalLB.length).toBe(0);
+	});
+
+	test("should return matches from the point of view of a participant", () => {
 		manager.create({
 			name: "Example",
 			tournamentId: 0,
@@ -199,34 +188,30 @@ FindDoubleElimination(
 
 		manager.update.match({ id: 0, opponent1: { result: "loss" } });
 		const afterFirstEliminated = manager.find.nextMatches(0, 1);
-		assert.equal(afterFirstEliminated.length, 1);
-		assert.equal(afterFirstEliminated[0].id, 3);
+		expect(afterFirstEliminated.length).toBe(1);
+		expect(afterFirstEliminated[0].id).toBe(3);
 		const afterFirstContinued = manager.find.nextMatches(0, 2);
-		assert.equal(afterFirstContinued.length, 1);
-		assert.equal(afterFirstContinued[0].id, 2);
+		expect(afterFirstContinued.length).toBe(1);
+		expect(afterFirstContinued[0].id).toBe(2);
 
 		manager.update.match({ id: 1, opponent1: { result: "win" } });
 		const beforeSemi1Up = manager.find.previousMatches(2, 2);
-		assert.equal(beforeSemi1Up.length, 1);
-		assert.equal(beforeSemi1Up[0].id, 0);
+		expect(beforeSemi1Up.length).toBe(1);
+		expect(beforeSemi1Up[0].id).toBe(0);
 
 		const beforeSemi1Down = manager.find.previousMatches(2, 3);
-		assert.equal(beforeSemi1Down.length, 1);
-		assert.equal(beforeSemi1Down[0].id, 1);
+		expect(beforeSemi1Down.length).toBe(1);
+		expect(beforeSemi1Down[0].id).toBe(1);
 
 		manager.update.match({ id: 3, opponent1: { result: "loss" } });
 		const afterLowerBracketEliminated = manager.find.nextMatches(3, 1);
-		assert.equal(afterLowerBracketEliminated.length, 0);
+		expect(afterLowerBracketEliminated.length).toBe(0);
 		const afterLowerBracketContinued = manager.find.nextMatches(3, 4);
-		assert.equal(afterLowerBracketContinued.length, 1);
-		assert.equal(afterLowerBracketContinued[0].id, 4);
+		expect(afterLowerBracketContinued.length).toBe(1);
+		expect(afterLowerBracketContinued[0].id).toBe(4);
 
-		assert.throws(
-			() => manager.find.nextMatches(3, 42),
+		expect(() => manager.find.nextMatches(3, 42)).toThrow(
 			"The participant does not belong to this match.",
 		);
-	},
-);
-
-FindSingleElimination.run();
-FindDoubleElimination.run();
+	});
+});
