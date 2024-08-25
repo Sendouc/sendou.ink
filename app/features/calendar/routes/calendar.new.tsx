d753365d5d5a344c492b174e0b9561e940063a5c
Kalle <38327916+Sendouc@users.noreply.github.com>
2024-08-25 10:32:34 +0300

    Allow up to 6 swiss groups

--- app/features/calendar/routes/calendar.new.tsx
+++ app/features/calendar/routes/calendar.new.tsx
@@ -1343,6 +1343,8 @@ function TournamentFormatSelector() {
 						<option value="2">2</option>
 						<option value="3">3</option>
 						<option value="4">4</option>
+						<option value="5">5</option>
+						<option value="6">6</option>
 					</select>
 				</div>
 			) : null}
