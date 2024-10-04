80b2c8d800677d1cabe3703961b8005ee03b1261
SSKarot <lovebailey74@outlook.com>
2024-10-05 01:46:08 +1000

    OCE Open badge + Tableturf rise badge

--- app/features/badges/homemade.ts
+++ app/features/badges/homemade.ts
@@ -28,6 +28,7 @@ export const homemadeBadges: BadgeInfo[] = [
 		displayName: "VERSUS THE WORLD",
 		fileName: "cookie",
 		authorDiscordId: "528851510222782474",
+
 	},
 	{
 		displayName: "Not Enough Liter",
@@ -44,4 +45,14 @@ export const homemadeBadges: BadgeInfo[] = [
 		fileName: "OriginalMessageDeleted",
 		authorDiscordId: "751912670403362836",
 	},
-];
+	{
+		displayName: "OCE Open",
+		fileName: "ausflag",
+		authorDiscordId: "1170249805373657093",
+	},
+	{
+		displayName: "TableTurf: RISE",
+		fileName: "Tableturf RISE",
+		authorDiscordId: "528851510222782474",
+	},
+]:
