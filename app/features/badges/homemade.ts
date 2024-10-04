711b370a9c5c019204ea18e15c9710ade82ecc4a
Kalle <38327916+Sendouc@users.noreply.github.com>
2024-10-04 22:55:46 +0300

    Lint

--- app/features/badges/homemade.ts
+++ app/features/badges/homemade.ts
@@ -44,7 +44,7 @@ export const homemadeBadges: BadgeInfo[] = [
 		fileName: "OriginalMessageDeleted",
 		authorDiscordId: "751912670403362836",
 	},
-  	{
+	{
 		displayName: "Shiny Wooper Achievement",
 		fileName: "shinywooper",
 		authorDiscordId: "528851510222782474",
