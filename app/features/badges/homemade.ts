dc57505531cb4a2bcc407f505e4b7e61533ccf58
marcushatesshot <marcusb2026@gmail.com>
2024-10-15 22:19:44 -0400

    Add Tristrike Tuesday: All Modes (Silver Bracket) badge

--- app/features/badges/homemade.ts
+++ app/features/badges/homemade.ts
@@ -15,6 +15,11 @@ export const homemadeBadges: BadgeInfo[] = [
 	// 	authorDiscordId: "123456789012345678",
 	// },
 	{
+		displayName: "Tristrike Tuesdays: All Modes (Silver Bracket)",
+		fileName: "sigma",
+		authorDiscordId: "789943264370884708",
+	},
+	{
 		displayName: "Treetop Tussle",
 		fileName: "tree",
 		authorDiscordId: "789943264370884708",
