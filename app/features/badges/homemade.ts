791467c8073088628703d47307298f0a87c553e0
marcushatesshot <marcusb2026@gmail.com>
2024-10-21 21:55:24 -0400

    Add Liftoff badge

--- app/features/badges/homemade.ts
+++ app/features/badges/homemade.ts
@@ -15,223 +15,224 @@ export const homemadeBadges: BadgeInfo[] = [
 	// 	authorDiscordId: "123456789012345678",
 	// },
 	{
-		displayName: "Tristrike Tuesdays: All Modes (Silver Bracket)",
-		fileName: "sigma",
-		authorDiscordId: "789943264370884708",
-	},
-	{
-		displayName: "Treetop Tussle",
-		fileName: "tree",
-		authorDiscordId: "789943264370884708",
-	},
-	{
-		displayName: "Treetop Tussle: Gold Edition",
-		fileName: "treegold",
-		authorDiscordId: "789943264370884708",
-	},
-	{
-		displayName: "Prize Fight",
-		fileName: "prizefight",
-		authorDiscordId: "789943264370884708",
-	},
-	{
-		displayName: "Prize Fight: Gold Edition",
-		fileName: "prizefightgold",
-		authorDiscordId: "789943264370884708",
-	},
-	{
-		displayName: "Break Zach's Bank",
-		fileName: "breakzachsbank",
-		authorDiscordId: "528851510222782474",
-	},
-	{
-		displayName: "Tristrike Tuesday",
-		fileName: "tristriketuesday",
-		authorDiscordId: "789943264370884708",
-	},
-	{
-		displayName: "VERSUS THE WORLD",
-		fileName: "cookie",
-		authorDiscordId: "528851510222782474",
-	},
-	{
-		displayName: "Not Enough Liter",
-		fileName: "custom_e-liter",
-		authorDiscordId: "352207524390240257",
-	},
-	{
-		displayName: "Blast Away!",
-		fileName: "lunablaster",
-		authorDiscordId: "342369454719631361",
-	},
-	{
-		displayName: "Original Message Deleted",
-		fileName: "original-message-deleted",
-		authorDiscordId: "751912670403362836",
-	},
-	{
-		displayName: "Shiny Wooper Achievement",
-		fileName: "shinywooper",
-		authorDiscordId: "528851510222782474",
-	},
-	{
-		displayName: "OCE Open",
-		fileName: "ausflag",
-		authorDiscordId: "1170249805373657093",
-	},
-	{
-		displayName: "TableTurf: RISE",
-		fileName: "tableturf-rise",
-		authorDiscordId: "528851510222782474",
-	},
-	{
-		displayName: "A Special Tournament",
-		fileName: "chara_special",
-		authorDiscordId: "1081428018666422313",
-	},
-	{
-		displayName: "Dolphin Showdown",
-		fileName: "dolphin",
-		authorDiscordId: "528851510222782474",
-	},
-	{
-		displayName: "Raid Weekend",
-		fileName: "raid-weekend",
-		authorDiscordId: "354880982890971136",
-	},
-	{
-		displayName: "It's Dangerous To Go Alone Master Bracket",
-		fileName: "idtgamb",
-		authorDiscordId: "354880982890971136",
-	},
-	{
-		displayName: "Lone Sailor",
-		fileName: "lone_sailor",
-		authorDiscordId: "354880982890971136",
-	},
-	{
-		displayName: "Junior's Draft",
-		fileName: "jrs_draft",
-		authorDiscordId: "118724226994667523",
-	},
-	{
-		displayName: "Paraflinch",
-		fileName: "paraflinch",
-		authorDiscordId: "528851510222782474",
-	},
-	{
-		displayName: "GHOST TYPE RUSH",
-		fileName: "gengar",
-		authorDiscordId: "528851510222782474",
-	},
-	{
-		displayName: "GHOST TYPE RUSH SHINY EDITION",
-		fileName: "gengarshiny",
-		authorDiscordId: "528851510222782474",
-	},
-	{
-		displayName: "DAYTONA Speedweeks '25",
-		fileName: "daytonaspeedweeks1st",
-		authorDiscordId: "528851510222782474",
-	},
-	{
-		displayName: "DAYTONA Speedweeks '25 (2nd place)",
-		fileName: "daytonaspeedweeks2nd",
-		authorDiscordId: "528851510222782474",
-	},
-	{
-		displayName: "Shark Tank (Gold)",
-		fileName: "sharktank1st",
-		authorDiscordId: "528851510222782474",
-	},
-	{
-		displayName: "Shark Tank (Silver)",
-		fileName: "sharktank2nd",
-		authorDiscordId: "528851510222782474",
-	},
-	{
-		displayName: "Shark Tank (Bronze)",
-		fileName: "sharktank3rd",
-		authorDiscordId: "528851510222782474",
-	},
-	{
-		displayName: "Power Pair Showdown",
-		fileName: "pps",
-		authorDiscordId: "789943264370884708",
-	},
-	{
-		displayName: "Splat'n'Go",
-		fileName: "splat-n-go",
-		authorDiscordId: "570323825669963972",
-	},
-	{
-		displayName: "Karot's Tourneys (100 KT Power)",
-		fileName: "ktlow",
-		authorDiscordId: "1170249805373657093",
-	},
-	{
-		displayName: "Karot's Tourneys (500 KT Power)",
-		fileName: "ktmed",
-		authorDiscordId: "1170249805373657093",
-	},
-	{
-		displayName: "Karot's Tourneys (999 KT Power)",
-		fileName: "kthigh",
-		authorDiscordId: "1170249805373657093",
-	},
-	{
-		displayName: "Heaven 2 Ocean Qualifer",
-		fileName: "heaven2ocean",
-		authorDiscordId: "528851510222782474",
-	},
-	{
-		displayName: "Seabattle",
-		fileName: "seabattle",
-		authorDiscordId: "528851510222782474",
-	},
-	{
-		displayName: "Dodge Rollers",
-		fileName: "splat_dualie",
-		authorDiscordId: "218883697007394816",
-	},
-	{
-		displayName: "Breeze LAN (1st)",
-		fileName: "breeze",
-		authorDiscordId: "342369454719631361",
-	},
-	{
-		displayName: "Breeze LAN (2nd)",
-		fileName: "breeze2",
-		authorDiscordId: "342369454719631361",
-	},
-	{
-		displayName: "Breeze LAN (3rd)",
-		fileName: "breeze3",
-		authorDiscordId: "342369454719631361",
-	},
-	{
-		displayName: "Splat Vibes LAN",
-		fileName: "vibesfirst",
-		authorDiscordId: "342369454719631361",
-	},
-	{
-		displayName: "Splat Vibes LAN (Top 4)",
-		fileName: "vibessecond",
-		authorDiscordId: "342369454719631361",
-	},
-	{
-		displayName: "Splat Legacy LAN",
-		fileName: "splatlegacy",
-		authorDiscordId: "342369454719631361",
-	},
-	{
-		displayName: "Ink Practice",
-		fileName: "inkpractice",
-		authorDiscordId: "342369454719631361",
-	},
-	{
-		displayName: "Breeze LAN (Salmon Run)",
-		fileName: "breezesr",
-		authorDiscordId: "342369454719631361",
-	},
-];
+			displayName: "Tristrike Tuesdays: All Modes (Silver Bracket)",
+			fileName: "sigma",
+			authorDiscordId: "789943264370884708",
+		},
+		{
+			displayName: "Treetop Tussle",
+			fileName: "tree",
+			authorDiscordId: "789943264370884708",
+		},
+		{
+			displayName: "Treetop Tussle: Gold Edition",
+			fileName: "treegold",
+			authorDiscordId: "789943264370884708",
+		},
+		{
+			displayName: "Prize Fight",
+			fileName: "prizefight",
+			authorDiscordId: "789943264370884708",
+		},
+		{
+			displayName: "Prize Fight: Gold Edition",
+			fileName: "prizefightgold",
+			authorDiscordId: "789943264370884708",
+		},
+		{
+			displayName: "Break Zach's Bank",
+			fileName: "breakzachsbank",
+			authorDiscordId: "528851510222782474",
+		},
+		{
+			displayName: "Tristrike Tuesday",
+			fileName: "tristriketuesday",
+			authorDiscordId: "789943264370884708",
+		},
+		{
+			displayName: "VERSUS THE WORLD",
+			fileName: "cookie",
+			authorDiscordId: "528851510222782474",
+		},
+		{
+			displayName: "Not Enough Liter",
+			fileName: "custom_e-liter",
+			authorDiscordId: "352207524390240257",
+		},
+		{
+			displayName: "Blast Away!",
+			fileName: "lunablaster",
+			authorDiscordId: "342369454719631361",
+		},
+		{
+			displayName: "Original Message Deleted",
+			fileName: "original-message-deleted",
+			authorDiscordId: "751912670403362836",
+		},
+		{
+			displayName: "Shiny Wooper Achievement",
+			fileName: "shinywooper",
+			authorDiscordId: "528851510222782474",
+		},
+		{
+			displayName: "OCE Open",
+			fileName: "ausflag",
+			authorDiscordId: "1170249805373657093",
+		},
+		{
+			displayName: "TableTurf: RISE",
+			fileName: "tableturf-rise",
+			authorDiscordId: "528851510222782474",
+		},
+		{
+			displayName: "A Special Tournament",
+			fileName: "chara_special",
+			authorDiscordId: "1081428018666422313",
+		},
+		{
+			displayName: "Dolphin Showdown",
+			fileName: "dolphin",
+			authorDiscordId: "528851510222782474",
+		},
+		{
+			displayName: "Raid Weekend",
+			fileName: "raid-weekend",
+			authorDiscordId: "354880982890971136",
+		},
+		{
+			displayName: "It's Dangerous To Go Alone Master Bracket",
+			fileName: "idtgamb",
+			authorDiscordId: "354880982890971136",
+		},
+		{
+			displayName: "Lone Sailor",
+			fileName: "lone_sailor",
+			authorDiscordId: "354880982890971136",
+		},
+		{
+			displayName: "Junior's Draft",
+			fileName: "jrs_draft",
+			authorDiscordId: "118724226994667523",
+		},
+		{
+			displayName: "Paraflinch",
+			fileName: "paraflinch",
+			authorDiscordId: "528851510222782474",
+		},
+		{
+			displayName: "GHOST TYPE RUSH",
+			fileName: "gengar",
+			authorDiscordId: "528851510222782474",
+		},
+		{
+			displayName: "GHOST TYPE RUSH SHINY EDITION",
+			fileName: "gengarshiny",
+			authorDiscordId: "528851510222782474",
+		},
+		{
+			displayName: "DAYTONA Speedweeks '25",
+			fileName: "daytonaspeedweeks1st",
+			authorDiscordId: "528851510222782474",
+		},
+		{
+			displayName: "DAYTONA Speedweeks '25 (2nd place)",
+			fileName: "daytonaspeedweeks2nd",
+			authorDiscordId: "528851510222782474",
+		},
+		{
+			displayName: "Shark Tank (Gold)",
+			fileName: "sharktank1st",
+			authorDiscordId: "528851510222782474",
+		},
+		{
+			displayName: "Shark Tank (Silver)",
+			fileName: "sharktank2nd",
+			authorDiscordId: "528851510222782474",
+		},
+		{
+			displayName: "Shark Tank (Bronze)",
+			fileName: "sharktank3rd",
+			authorDiscordId: "528851510222782474",
+		},
+		{
+			displayName: "Power Pair Showdown",
+			fileName: "pps",
+			authorDiscordId: "789943264370884708",
+		},
+		{
+			displayName: "Splat'n'Go",
+			fileName: "splat-n-go",
+			authorDiscordId: "570323825669963972",
+		},
+		{
+			displayName: "Karot's Tourneys (100 KT Power)",
+			fileName: "ktlow",
+			authorDiscordId: "1170249805373657093",
+		},
+		{
+			displayName: "Karot's Tourneys (500 KT Power)",
+			fileName: "ktmed",
+			authorDiscordId: "1170249805373657093",
+		},
+		{
+			displayName: "Karot's Tourneys (999 KT Power)",
+			fileName: "kthigh",
+			authorDiscordId: "1170249805373657093",
+		},
+		{
+			displayName: "Heaven 2 Ocean Qualifer",
+			fileName: "heaven2ocean",
+			authorDiscordId: "528851510222782474",
+		},
+		{
+			displayName: "Seabattle",
+			fileName: "seabattle",
+			authorDiscordId: "528851510222782474",
+		},
+		{
+			displayName: "Dodge Rollers",
+			fileName: "splat_dualie",
+			authorDiscordId: "218883697007394816",
+		},
+		{
+			displayName: "Breeze LAN (1st)",
+			fileName: "breeze",
+			authorDiscordId: "342369454719631361",
+		},
+		{
+			displayName: "Breeze LAN (2nd)",
+			fileName: "breeze2",
+			authorDiscordId: "342369454719631361",
+		},
+		{
+			displayName: "Breeze LAN (3rd)",
+			fileName: "breeze3",
+			authorDiscordId: "342369454719631361",
+		},
+		{
+			displayName: "Splat Vibes LAN",
+			fileName: "vibesfirst",
+			authorDiscordId: "342369454719631361",
+		},
+		{
+			displayName: "Splat Vibes LAN (Top 4)",
+			fileName: "vibessecond",
+			authorDiscordId: "342369454719631361",
+		},
+		{
+			displayName: "Splat Legacy LAN",
+			fileName: "splatlegacy",
+			authorDiscordId: "342369454719631361",
+		},
+		{
+			displayName: "Ink Practice",
+			fileName: "inkpractice",
+			authorDiscordId: "342369454719631361",
+		},
+		{
+			displayName: "Breeze LAN (Salmon Run)",
+			fileName: "breezesr",
+			authorDiscordId: "342369454719631361",
+		},
+	];
+	
